857b871c67e92a37783a0b8eeee94bfa4061a6d6
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-04-05 17:42:29 +0900

    test: test

--- payment-api/src/main/java/org/ecommerce/paymentapi/PaymentApiApplication.java
+++ payment-api/src/main/java/org/ecommerce/paymentapi/PaymentApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * test
+ * test1
  */
 @SpringBootApplication
 public class PaymentApiApplication {
