7ae6fbca481e35b16b63a41fb2543489c9a7c52a
HongJongMin <gwa7417@gmail.com>
2024-04-07 16:20:41 +0900

    refactor: 코드 수정
    [Enum 들여쓰기 수정]

--- user-api/src/main/java/org/ecommerce/userapi/entity/type/Status.java
+++ user-api/src/main/java/org/ecommerce/userapi/entity/type/Status.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum Status {
-		GENERAL("일반"),
-		WITHDRAWAL("탈퇴"),
-		SUSPEND("정지");
-		private final String value;
+
+	GENERAL("일반"),
+	WITHDRAWAL("탈퇴"),
+	SUSPEND("정지");
+
+	private final String value;
 
 	Status(String value) {
 		this.value = value;
