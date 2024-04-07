bc08ac6a099c5961ba3020e5bc2b2ad46e4c16f0
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-04-07 21:52:59 +0900

    Feature: H2 Database 세팅

--- payment-api/src/main/java/org/ecommerce/paymentapi/PaymentApiApplication.java
+++ payment-api/src/main/java/org/ecommerce/paymentapi/PaymentApiApplication.java
@@ -3,14 +3,11 @@ package org.ecommerce.paymentapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * test1
- */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.ecommerce.common","org.ecommerce.paymentapi"})
 public class PaymentApiApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(PaymentApiApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApiApplication.class, args);
+	}
 
 }
