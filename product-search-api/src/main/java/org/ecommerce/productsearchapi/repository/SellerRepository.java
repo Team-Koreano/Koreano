ca8b1b27d28f2d5205692cbe21587d2313f4f479
no.oneho <oneho.dev@gmail.com>
2024-04-07 17:04:03 +0900

    Refactor: SellerRepository에 @Repository 어노테이션 누락 해결

--- product-search-api/src/main/java/org/ecommerce/productsearchapi/repository/SellerRepository.java
+++ product-search-api/src/main/java/org/ecommerce/productsearchapi/repository/SellerRepository.java
@@ -2,6 +2,8 @@ package org.ecommerce.productsearchapi.repository;
 
 import org.ecommerce.productsearchapi.entity.SellerRep;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SellerRepository extends JpaRepository<SellerRep, Integer> {
 }
