92d66842ee7ee748789ed113ab3858daabaa4e28
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-04-07 21:56:22 +0900

    Feature: Dto 클래스 생성

---
+++ payment-api/src/main/java/org/ecommerce/paymentapi/dto/PaymentDto.java
@@ -0,0 +1,11 @@
+package org.ecommerce.paymentapi.dto;
+
+import org.ecommerce.paymentapi.entity.Payment;
+
+public class PaymentDto {
+	public static class Request {
+	}
+
+	public record Response() {
+	}
+}
