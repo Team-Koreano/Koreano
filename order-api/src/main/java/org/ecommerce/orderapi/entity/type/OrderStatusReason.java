7dafae44f3c3cff0af4568d4cd8feb1a439b8ee4
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-08 17:06:03 +0900

    Feat : 주문 상태 관련 Enum 작성

---
+++ order-api/src/main/java/org/ecommerce/orderapi/entity/type/OrderStatusReason.java
@@ -0,0 +1,27 @@
+package org.ecommerce.orderapi.entity.type;
+
+import org.ecommerce.common.utils.mapper.EnumMapperType;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum OrderStatusReason implements EnumMapperType {
+
+	REFUND("환불"),
+	FAILED_PAYMENT("결제실패"),
+	EXCHANGE("교환"),
+	SIMPLE_CHANGE_OF_HEART("단순변심")
+	;
+
+	private final String title;
+
+	@Override
+	public String getCode() {
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		return null;
+	}
+}
