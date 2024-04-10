d95406552f896014c26612686f25f853e6694901
no.oneho <oneho.dev@gmail.com>
2024-04-11 04:49:37 +0900

    Refactor: SellerRep @Builder 제거

--- product/src/main/java/org/ecommerce/product/entity/SellerRep.java
+++ product/src/main/java/org/ecommerce/product/entity/SellerRep.java
@@ -7,13 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "seller_rep")
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
