e10a9df85cb0eb63e04b9631073691669ebc583e
HongJongMin <gwa7417@gmail.com>
2024-04-10 20:46:38 +0900

    feat: 유저 Controller, Dto, Service 테스트 코드 작성 및 확인

---
+++ user-api/src/test/java/org/ecommerce/userapi/controller/UserControllerTest.java
@@ -0,0 +1,106 @@
+package org.ecommerce.userapi.controller;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.assertj.core.api.Assertions;
+import org.ecommerce.common.vo.Response;
+import org.ecommerce.userapi.dto.UserDto;
+import org.ecommerce.userapi.entity.type.Gender;
+import org.ecommerce.userapi.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(UserController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private UserService userService;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@BeforeEach
+	public void setup() {
+		this.mockMvc = MockMvcBuilders
+			.webAppContextSetup(context)
+			.addFilter(new CharacterEncodingFilter("UTF-8", true))
+			.apply(springSecurity())
+			.build();
+	}
+
+	private ResultActions performPostRequest(String content) throws Exception {
+		return mockMvc.perform(post("/user/register")
+			.with(csrf())
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(content)
+			.with(user("test")));
+	}
+
+	@Test
+	void 회원등록() throws Exception {
+		//given
+		UserDto.Request.Register registerRequest = UserDto.Request.Register.builder()
+			.email("test@example.com")
+			.name("Test User")
+			.password("password")
+			.gender(Gender.MALE)
+			.age((short)30)
+			.phoneNumber("01012345678")
+			.build();
+
+		UserDto.Response.Register expectedResponse = UserDto.Response.Register.builder()
+			.email(registerRequest.email())
+			.name(registerRequest.name())
+			.age(registerRequest.age())
+			.gender(registerRequest.gender())
+			.phoneNumber(registerRequest.phoneNumber())
+			.build();
+
+		String content = objectMapper.writeValueAsString(registerRequest);
+
+		when(userService.registerUser(registerRequest)).thenReturn(expectedResponse);
+
+		//when
+		final ResultActions resultActions = performPostRequest(content);
+
+		//then
+		final MvcResult mvcResult = resultActions.andExpect(status().isOk())
+			.andReturn();
+
+		UserDto.Response.Register result = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(),
+			new TypeReference<Response<UserDto.Response.Register>>() {
+			}
+		).result();
+
+		Assertions.assertThat(result).isEqualTo(expectedResponse);
+
+	}
+}
---
+++ user-api/src/test/java/org/ecommerce/userapi/service/UserServiceTest.java
@@ -0,0 +1,129 @@
+package org.ecommerce.userapi.service;
+
+import static org.mockito.Mockito.*;
+
+import org.assertj.core.api.Assertions;
+import org.ecommerce.common.error.CustomException;
+import org.ecommerce.userapi.dto.UserDto;
+import org.ecommerce.userapi.entity.Users;
+import org.ecommerce.userapi.entity.type.Gender;
+import org.ecommerce.userapi.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+	@InjectMocks
+	private UserService userService;
+
+	@Mock
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@BeforeEach
+	public void 초기셋팅() {
+		Users user1 = Users.create(
+			"user1@example.com",
+			"John Doe",
+			"password1",
+			Gender.MALE,
+			(short)25,
+			"01012345678"
+		);
+		Users user2 = Users.create(
+			"user2@example.com",
+			"Jane Smith",
+			"password2",
+			Gender.FEMALE,
+			(short)30,
+			"01087654321"
+		);
+		Users user3 = Users.create(
+			"user3@example.com",
+			"Bob Johnson",
+			"password3",
+			Gender.MALE,
+			(short)35,
+			"01099876543"
+		);
+		userRepository.save(user1);
+		userRepository.save(user2);
+		userRepository.save(user3);
+	}
+
+	@Test
+	void 회원등록() {
+		//given
+		UserDto.Request.Register newUserRequest = UserDto.Request.Register.builder()
+			.email("newuser@example.com")
+			.name("New User")
+			.password("newpassword")
+			.gender(Gender.MALE)
+			.age((short)40)
+			.phoneNumber("01012341234")
+			.build();
+
+		//when
+		when(userRepository.existsByEmail(newUserRequest.email())).thenReturn(false);
+		when(userRepository.existsByPhoneNumber(newUserRequest.phoneNumber())).thenReturn(false);
+
+		UserDto.Response.Register response = userService.registerUser(newUserRequest);
+
+		Users savedUser = Users.create(
+			newUserRequest.email(),
+			newUserRequest.name(),
+			bCryptPasswordEncoder.encode(newUserRequest.password()),
+			newUserRequest.gender(),
+			newUserRequest.age(),
+			newUserRequest.phoneNumber()
+		);
+
+		//then
+		Assertions.assertThat(response).isEqualTo(UserDto.Response.Register.of(savedUser));
+
+		// given
+		// 중복 이메일 케이스
+		UserDto.Request.Register duplicatedEmailRequest = UserDto.Request.Register.builder()
+			.email("user3@example.com")
+			.name("Duplicate Email")
+			.password("password")
+			.gender(Gender.FEMALE)
+			.age((short)25)
+			.phoneNumber("01012345678")
+			.build();
+
+		//when
+		when(userRepository.existsByEmail(duplicatedEmailRequest.email())).thenReturn(true);
+
+		//then
+		Assertions.assertThatThrownBy(() -> userService.registerUser(duplicatedEmailRequest))
+			.isInstanceOf(CustomException.class);
+
+		//given
+		// 중복 전화번호 케이스
+		UserDto.Request.Register duplicatedPhoneRequest = UserDto.Request.Register.builder()
+			.email("newuser2@example.com")
+			.name("Duplicate Phone")
+			.password("password")
+			.gender(Gender.MALE)
+			.age((short)30)
+			.phoneNumber("01099876543")
+			.build();
+
+		//when
+		when(userRepository.existsByEmail(duplicatedPhoneRequest.email())).thenReturn(false);
+		when(userRepository.existsByPhoneNumber(duplicatedPhoneRequest.phoneNumber())).thenReturn(true);
+
+		//then
+		Assertions.assertThatThrownBy(() -> userService.registerUser(duplicatedPhoneRequest))
+			.isInstanceOf(CustomException.class);
+	}
+}
