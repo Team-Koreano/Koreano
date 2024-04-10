e6bbdec2362044a419fba9d075e04dd1facca6a0
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-10 13:22:15 +0900

    Feat : 장바구니 담기 기능 Controller, DTO 구현 #35 validation 의존성 추가

--- bucket-api/src/main/java/org/ecommerce/bucketapi/controller/BucketController.java
+++ bucket-api/src/main/java/org/ecommerce/bucketapi/controller/BucketController.java
@@ -6,9 +6,12 @@ import org.ecommerce.bucketapi.dto.BucketDto;
 import org.ecommerce.bucketapi.service.BucketService;
 import org.ecommerce.common.vo.Response;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,10 +21,21 @@ public class BucketController {
 
 	private final BucketService bucketService;
 
+	// todo jwt 도입 후 로직 변경
+	private final static Integer USER_ID = 1;
+
 	@GetMapping
 	public Response<List<BucketDto.Response>> getBuckets() {
-		final Integer userId = 1;
-		final List<BucketDto.Response> bucketResponse = bucketService.getAllBuckets(userId);
+		final List<BucketDto.Response> bucketResponse = bucketService.getAllBuckets(USER_ID);
+		return new Response<>(200, bucketResponse);
+	}
+
+	@PostMapping
+	public Response<BucketDto.Response> addBucket(
+		@RequestBody @Valid final BucketDto.Request.Add addRequest
+	) {
+		bucketService.validateBucketByUser(USER_ID, addRequest.productId());
+		final BucketDto.Response bucketResponse = bucketService.addBucket(USER_ID, addRequest);
 		return new Response<>(200, bucketResponse);
 	}
 }
