071427dfd2d39af22c4b413c68b40cef4b4f8728
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-10 13:23:00 +0900

    Feat : 장바구니 담기, Service 구현 #35

--- bucket-api/src/main/java/org/ecommerce/bucketapi/entity/Bucket.java
+++ bucket-api/src/main/java/org/ecommerce/bucketapi/entity/Bucket.java
@@ -1,13 +1,18 @@
 package org.ecommerce.bucketapi.entity;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.time.LocalDate;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "bucket")
@@ -16,20 +21,37 @@ import java.time.LocalDate;
 @Getter
 public class Bucket {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(name = "user_id")
-    private Integer userId;
+	@Column(name = "user_id")
+	private Integer userId;
 
-    @Column(name = "product_id")
-    private Integer productId;
+	@Column(name = "seller")
+	private String seller;
 
-    @Column(name = "quantity")
-    private Integer quantity;
+	@Column(name = "product_id")
+	private Integer productId;
 
-    @CreationTimestamp
-    @Column(name = "create_date")
-    private LocalDate createDate;
+	@Column(name = "quantity")
+	private Integer quantity;
+
+	@CreationTimestamp
+	@Column(name = "create_date")
+	private LocalDate createDate;
+
+	public static Bucket ofAdd(
+		final Integer userId,
+		final String seller,
+		final Integer productId,
+		final Integer quantity
+	) {
+		Bucket bucket = new Bucket();
+		bucket.userId = userId;
+		bucket.seller = seller;
+		bucket.productId = productId;
+		bucket.quantity = quantity;
+		return bucket;
+	}
 }
--- bucket-api/src/main/java/org/ecommerce/bucketapi/service/BucketService.java
+++ bucket-api/src/main/java/org/ecommerce/bucketapi/service/BucketService.java
@@ -15,10 +15,32 @@ public class BucketService {
 
 	private final BucketRepository bucketRepository;
 
+	// TODO : 회원 검증 로직 추가 (crud)
+	// TODO : 상품 검증 로직 추가 (crud)
+	// TODO : Rebase 이후 Exception Refactoring
+
+	public void validateBucketByUser(final Integer userId, final Integer productId) {
+		if (bucketRepository.existsByUserIdAndProductId(userId, productId)) {
+			throw new RuntimeException("이미 장바구니에 존재하는 상품입니다.");
+		}
+	}
+
 	public List<BucketDto.Response> getAllBuckets(final Integer userId) {
 		final List<Bucket> buckets = bucketRepository.findAllByUserId(userId);
 		return buckets.stream()
 			.map(BucketDto.Response::of)
 			.toList();
+	}
+
+	public BucketDto.Response addBucket(final Integer userId, final BucketDto.Request.Add addRequest) {
+		final Bucket newBucket = Bucket.ofAdd(
+			userId,
+			addRequest.seller(),
+			addRequest.productId(),
+			addRequest.quantity()
+		);
+
+		bucketRepository.save(newBucket);
+		return BucketDto.Response.of(newBucket);
 	}
 }
