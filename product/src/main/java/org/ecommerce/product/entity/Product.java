40583c591cc5c4b1318e008bf1fcdbb507b553a4
no.oneho <oneho.dev@gmail.com>
2024-04-11 04:35:02 +0900

    Refactor: @Builder 제거

--- product/src/main/java/org/ecommerce/product/entity/Product.java
+++ product/src/main/java/org/ecommerce/product/entity/Product.java
@@ -21,13 +21,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "product")
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
