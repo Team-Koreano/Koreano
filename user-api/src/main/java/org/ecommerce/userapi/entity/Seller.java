80cb1f9e40bd48cbd6ed3e2f6b58cd6d1cf77431
HongJongMin <gwa7417@gmail.com>
2024-04-10 20:43:25 +0900

    feat: 생성자 추가

--- user-api/src/main/java/org/ecommerce/userapi/entity/Seller.java
+++ user-api/src/main/java/org/ecommerce/userapi/entity/Seller.java
@@ -13,11 +13,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "seller")
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Seller {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
