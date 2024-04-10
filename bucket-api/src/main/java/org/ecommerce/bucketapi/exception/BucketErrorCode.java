b0d1e0ce9d28611cf6ae61e46d41b620f929c0f3
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-11 01:19:07 +0900

    Refactor : BucketErrorCode 수정 Lombok의 Getter Annotation을 추가하고, 기존 get() 메소드 삭제

--- bucket-api/src/main/java/org/ecommerce/bucketapi/exception/BucketErrorCode.java
+++ bucket-api/src/main/java/org/ecommerce/bucketapi/exception/BucketErrorCode.java
@@ -2,20 +2,13 @@ package org.ecommerce.bucketapi.exception;
 
 import org.ecommerce.common.error.ErrorCode;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public enum BucketErrorCode implements ErrorCode {
 	;
 	private final int code;
 	private final String message;
-
-	@Override
-	public int getCode() {
-		return this.code;
-	}
-	@Override
-	public String getMessage() {
-		return this.message;
-	}
 }
