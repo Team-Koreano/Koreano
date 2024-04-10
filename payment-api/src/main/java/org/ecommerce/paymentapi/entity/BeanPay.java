0202ecb4cde7215e25ff31c016d5f0946e30ea23
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-04-10 20:02:14 +0900

    Refactor: #33 빈페이 사전결제 클래스 수정

--- payment-api/src/main/java/org/ecommerce/paymentapi/entity/BeanPay.java
+++ payment-api/src/main/java/org/ecommerce/paymentapi/entity/BeanPay.java
@@ -3,6 +3,7 @@ package org.ecommerce.paymentapi.entity;
 import java.time.LocalDateTime;
 
 import org.ecommerce.paymentapi.entity.type.BeanPayStatus;
+import org.ecommerce.paymentapi.entity.type.ProcessStatus;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -15,19 +16,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Getter
 @Entity
 @Table(name = "beanpay")
 public class BeanPay {
 
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "user_id")
@@ -38,11 +38,25 @@ public class BeanPay {
 	private Integer amount;
 
 	@Enumerated(EnumType.STRING)
-	@Column(name = "status", nullable = false)
-	private BeanPayStatus status;
+	@Column(name = "beanpay_status", nullable = false)
+	private BeanPayStatus beanPayStatus;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "process_status", nullable = false)
+	private ProcessStatus processStatus;
 
 	@CreationTimestamp
 	@Column(name = "create_datetime", updatable = false)
 	private LocalDateTime createDateTime;
+
+	public static BeanPay ofCreate(Integer userId, Integer amount) {
+		BeanPay beanPay = new BeanPay();
+		beanPay.userId = userId;
+		beanPay.amount = amount;
+		beanPay.beanPayStatus = BeanPayStatus.DEPOSIT;
+		beanPay.processStatus = ProcessStatus.PENDING;
+		beanPay.createDateTime = LocalDateTime.now();
+		return beanPay;
+	}
 
 }
