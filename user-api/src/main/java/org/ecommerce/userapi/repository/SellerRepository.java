4c9064b049a14a6ba6208304d89fbc6479553a08
HongJongMin <gwa7417@gmail.com>
2024-04-10 14:26:11 +0900

    feat: 셀러 Dto, Repository 생성

---
+++ user-api/src/main/java/org/ecommerce/userapi/repository/SellerRepository.java
@@ -0,0 +1,10 @@
+package org.ecommerce.userapi.repository;
+
+import org.ecommerce.userapi.entity.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, Integer> {
+	boolean existsByEmail(String email);
+}
