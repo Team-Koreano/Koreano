2e0fb8f38ee98301b8cdb1011834bfb67c01ddb9
no.oneho <oneho.dev@gmail.com>
2024-04-10 15:59:02 +0900

    Test: ApplicationTests.Java 에 대상 클래스 명시

--- product-search-api/src/test/java/org/ecommerce/productsearchapi/ProductSearchApiApplicationTests.java
+++ product-search-api/src/test/java/org/ecommerce/productsearchapi/ProductSearchApiApplicationTests.java
@@ -3,7 +3,7 @@ package org.ecommerce.productsearchapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProductSearchApiApplication.class)
 class ProductSearchApiApplicationTests {
 
   @Test
