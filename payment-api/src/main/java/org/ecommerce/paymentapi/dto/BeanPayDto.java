9d2e661b6f51208f9240e3732154d8f044505525
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-04-10 21:31:38 +0900

    Feat: #33 사전충전객체를 생성하기 위한 기능 개발

---
+++ payment-api/src/main/java/org/ecommerce/paymentapi/dto/BeanPayDto.java
@@ -0,0 +1,38 @@
+package org.ecommerce.paymentapi.dto;
+
+import java.time.LocalDateTime;
+
+import org.antlr.v4.runtime.misc.NotNull;
+import org.ecommerce.paymentapi.entity.BeanPay;
+import org.ecommerce.paymentapi.entity.type.BeanPayStatus;
+import org.ecommerce.paymentapi.entity.type.ProcessStatus;
+import org.springframework.validation.annotation.Validated;
+
+public class BeanPayDto {
+	public static class Request {
+		public record PreCharge(
+			Integer userId,
+			Integer amount
+		) {
+		}
+
+
+	}
+
+	public record Response(Long beanPayId,
+						   Integer userId,
+						   Integer amount,
+						   BeanPayStatus beanPayStatus,
+						   ProcessStatus processStatus,
+						   LocalDateTime createDateTime) {
+
+		public static BeanPayDto.Response ofCreate(BeanPay beanPay) {
+			return new BeanPayDto.Response(beanPay.getId(),
+				beanPay.getUserId(),
+				beanPay.getAmount(),
+				beanPay.getBeanPayStatus(),
+				beanPay.getProcessStatus(),
+				beanPay.getCreateDateTime());
+		}
+	}
+}
