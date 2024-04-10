2f395ff2e9cbd810e3b27b762185cd56098f6a80
no.oneho <oneho.dev@gmail.com>
2024-04-11 05:11:20 +0900

    Refactor: 현재 데이터 베이스 미연동으로 인해 exclude = {DataSourceAutoConfiguration.class} 추가

--- product/src/main/java/org/ecommerce/product/ProductApplication.java
+++ product/src/main/java/org/ecommerce/product/ProductApplication.java
@@ -2,8 +2,10 @@ package org.ecommerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//TODO 데이터베이스 연동 시 제거
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ProductApplication {
 
 	public static void main(String[] args) {
