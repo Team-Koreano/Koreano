471d62db71c1ca34312a48854174b840bb1f54a9
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-10 17:58:39 +0900

    chore : scanBasePackages 추가

--- bucket-api/src/main/java/org/ecommerce/bucketapi/BucketApiApplication.java
+++ bucket-api/src/main/java/org/ecommerce/bucketapi/BucketApiApplication.java
@@ -3,7 +3,7 @@ package org.ecommerce.bucketapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.ecommerce.common","org.ecommerce.bucketapi"})
 public class BucketApiApplication {
 
   public static void main(String[] args) {
