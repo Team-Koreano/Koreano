bb82dca5fbdc604725e38665f4778471ed867c01
HongJongMin <gwa7417@gmail.com>
2024-04-11 09:32:07 +0900

    refactor: @getter 추가

--- user-api/src/main/java/org/ecommerce/userapi/exception/UserErrorCode.java
+++ user-api/src/main/java/org/ecommerce/userapi/exception/UserErrorCode.java
@@ -3,6 +3,9 @@ package org.ecommerce.userapi.exception;
 import org.ecommerce.common.error.ErrorCode;
 import org.springframework.http.HttpStatus;
 
+import lombok.Getter;
+
+@Getter
 public enum UserErrorCode implements ErrorCode {
 	DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST,"중복된 이메일 입니다"),
 	DUPLICATED_PHONENUMBER(HttpStatus.BAD_REQUEST,"중복된 전화번호 입니다")
@@ -10,19 +13,9 @@ public enum UserErrorCode implements ErrorCode {
 
 	UserErrorCode(HttpStatus status, String message) {
 		this.message = message;
-		this.status = status.value();
+		this.code = status.value();
 	}
 
 	private final String message;
-	private final int status;
-
-	@Override
-	public int getCode() {
-		return status;
-	}
-
-	@Override
-	public String getMessage() {
-		return message;
-	}
+	private final int code;
 }
