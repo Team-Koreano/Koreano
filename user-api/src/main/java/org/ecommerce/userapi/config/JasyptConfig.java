9c1f09a453164c08eef4aba35eda4377ee83f9af
HongJongMin <gwa7417@gmail.com>
2024-04-15 03:54:45 +0900

    feat: jasypt Config 구성

---
+++ user-api/src/main/java/org/ecommerce/userapi/config/JasyptConfig.java
@@ -0,0 +1,32 @@
+package org.ecommerce.userapi.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class JasyptConfig {
+
+	@Value("${jasypt.encryptor.password}")
+	private String secretKeySource;
+
+	@Bean(name = "jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword(secretKeySource);
+		config.setAlgorithm("PBEWithMD5AndDES"); //암호화 알고리즘
+		config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
+		config.setPoolSize("1"); // 인스턴스 pool
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+		return encryptor;
+	}
+}
