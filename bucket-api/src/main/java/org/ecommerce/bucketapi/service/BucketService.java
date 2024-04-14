dba26403a25f445dee445d8bb95049903e61d9c0
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-14 14:35:04 +0900

    Refactor : BucketService 수정 validateBucketByUser메소드 Transaction 옵션 추가, 들여쓰기 밑 줄 바꿈 수정

--- bucket-api/src/main/java/org/ecommerce/bucketapi/service/BucketService.java
+++ bucket-api/src/main/java/org/ecommerce/bucketapi/service/BucketService.java
@@ -1,10 +1,11 @@
 package org.ecommerce.bucketapi.service;
 
+import static org.ecommerce.bucketapi.exception.BucketErrorCode.*;
+
 import java.util.List;
 
 import org.ecommerce.bucketapi.dto.BucketDto;
 import org.ecommerce.bucketapi.entity.Bucket;
-import org.ecommerce.bucketapi.exception.BucketErrorCode;
 import org.ecommerce.bucketapi.repository.BucketRepository;
 import org.ecommerce.common.error.CustomException;
 import org.springframework.stereotype.Service;
@@ -22,36 +23,44 @@ public class BucketService {
 	// TODO : 회원 검증 로직 추가 (crud)
 	// TODO : 상품 검증 로직 추가 (cu)
 
+	@Transactional(readOnly = true)
 	public void validateBucketByUser(final Integer userId, final Long bucketId) {
 		if (!bucketRepository.existsByUserIdAndId(userId, bucketId)) {
-			throw new CustomException(BucketErrorCode.INVALID_BUCKET_WITH_USER);
+			throw new CustomException(INVALID_BUCKET_WITH_USER);
 		}
 	}
 
 	@Transactional(readOnly = true)
 	public List<BucketDto.Response> getAllBuckets(final Integer userId) {
-		final List<Bucket> buckets = bucketRepository.findAllByUserId(userId);
-		return buckets.stream()
-			.map(BucketDto.Response::of)
-			.toList();
+		return bucketRepository.findAllByUserId(userId)
+				.stream()
+				.map(BucketDto.Response::of)
+				.toList();
 	}
 
-	public BucketDto.Response addBucket(final Integer userId, final BucketDto.Request.Add addRequest) {
+	public BucketDto.Response addBucket(
+			final Integer userId,
+			final BucketDto.Request.Add addRequest
+	) {
 		final Bucket newBucket = Bucket.ofAdd(
-			userId,
-			addRequest.seller(),
-			addRequest.productId(),
-			addRequest.quantity()
+				userId,
+				addRequest.seller(),
+				addRequest.productId(),
+				addRequest.quantity()
 		);
+
 		final Bucket bucket = bucketRepository.save(newBucket);
 		return BucketDto.Response.of(bucket);
 	}
 
-	public BucketDto.Response updateBucket(final Long bucketId, final BucketDto.Request.Update updateRequest) {
-		final Bucket bucket = bucketRepository.findById(bucketId).orElseThrow(
-			() -> new CustomException(BucketErrorCode.NOT_FOUND_BUCKET_ID)
-		);
-		bucket.update(updateRequest);
+	public BucketDto.Response updateBucket(
+			final Long bucketId,
+			final BucketDto.Request.Update updateRequest
+	) {
+		final Bucket bucket = bucketRepository.findById(bucketId)
+				.orElseThrow(() -> new CustomException(NOT_FOUND_BUCKET_ID));
+
+		bucket.update(updateRequest.quantity());
 		return BucketDto.Response.of(bucket);
 	}
 }
