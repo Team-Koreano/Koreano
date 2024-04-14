6a0db3e031ccd10326b64669589748bcd5a58f82
HongJongMin <gwa7417@gmail.com>
2024-04-15 03:56:50 +0900

    feat: Redis 설정과 Template 커스텀하여 메서드 구현

---
+++ user-api/src/main/java/org/ecommerce/userapi/config/RedisConfig.java
@@ -0,0 +1,42 @@
+package org.ecommerce.userapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+@EnableRedisRepositories
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String redisHost;
+
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory(){
+		return new LettuceConnectionFactory(redisHost,redisPort);
+	}
+
+	@Bean
+	public RedisTemplate<?,?> redisTemplate(){
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+		redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		return redisTemplate;
+	}
+}
