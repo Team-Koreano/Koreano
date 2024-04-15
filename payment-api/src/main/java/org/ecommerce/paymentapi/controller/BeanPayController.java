4e6164e9efa4c71c8f8c51bd0b0ed01851e3f697
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-04-15 15:32:29 +0900

    Refactor: api versioning
    api에 버전까지 확인하기 위해 url path를 변경
    
    자원을 생성하는 로직이라 get이 아닌 post가 적절하여 수정

--- payment-api/src/main/java/org/ecommerce/paymentapi/controller/BeanPayController.java
+++ payment-api/src/main/java/org/ecommerce/paymentapi/controller/BeanPayController.java
@@ -5,22 +5,24 @@ import org.ecommerce.paymentapi.dto.BeanPayDto;
 import org.ecommerce.paymentapi.service.BeanPayService;
 import org.ecommerce.paymentapi.service.PaymentServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/beanpay")
+@RequestMapping("/api/beanpay/v1")
 public class BeanPayController {
 	private final PaymentServiceImpl paymentService;
 	private final BeanPayService beanPayService;
 
-	@GetMapping("/payments")
+	@PostMapping("/payments")
 	public Response<BeanPayDto.Response> preCharge(@RequestBody final BeanPayDto.Request.PreCharge request) {
 		final BeanPayDto.Response preCharge = beanPayService.preChargeBeanPay(request);
 		return new Response<>(200, preCharge);
