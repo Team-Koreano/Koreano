1ed43fa530b90ec188faa7dfa4228ec7c40dfc4b
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-15 17:52:44 +0900

    Feat : OpenFeign 세팅 #54

--- order-api/src/main/java/org/ecommerce/orderapi/OrderApiApplication.java
+++ order-api/src/main/java/org/ecommerce/orderapi/OrderApiApplication.java
@@ -2,8 +2,10 @@ package org.ecommerce.orderapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderApiApplication {
 
   public static void main(String[] args) {
