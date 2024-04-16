b69c19102ebd87206fb35f8988af377b9ed2f184
HongJongMin <gwa7417@gmail.com>
2024-04-16 20:52:36 +0900

    refactor: 공통 로직에 status 추가

--- user-api/src/main/java/org/ecommerce/userapi/entity/Member.java
+++ user-api/src/main/java/org/ecommerce/userapi/entity/Member.java
@@ -1,6 +1,9 @@
 package org.ecommerce.userapi.entity;
 
+import org.ecommerce.userapi.entity.type.UserStatus;
+
 public interface Member {
 	Integer getId();
 	String getEmail();
+	UserStatus getUserStatus();
 }
