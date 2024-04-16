97c83dc33f7b3c57d33a9170b8aa02463ddad385
HongJongMin <gwa7417@gmail.com>
2024-04-16 22:17:18 +0900

    refactor: 인코딩 상수로 변경, Map -> Response 구조로 변경

--- user-api/src/main/java/org/ecommerce/userapi/security/custom/ResponseConfigurer.java
+++ user-api/src/main/java/org/ecommerce/userapi/security/custom/ResponseConfigurer.java
@@ -1,8 +1,9 @@
 package org.ecommerce.userapi.security.custom;
 
 import java.io.IOException;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
+import org.ecommerce.common.vo.Response;
 import org.ecommerce.userapi.exception.UserErrorCode;
 import org.springframework.http.MediaType;
 
@@ -14,11 +15,10 @@ public interface ResponseConfigurer {
 	default void responseSetting(HttpServletResponse response, UserErrorCode errorCode) throws IOException {
 		response.setStatus(errorCode.getCode());
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-		response.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
-		Map<String, Object> responseBody = Map.of(
-			"status", errorCode.getCode(),
-			"result", errorCode.getMessage()
+		Response responseBody = new Response<>(
+			errorCode.getCode(), errorCode.getMessage()
 		);
 
 		response.getWriter().write(new ObjectMapper().writeValueAsString(responseBody));
