73976d6dc98eb9eedb51f70b36b06c5c9c155693
HongJongMin <gwa7417@gmail.com>
2024-04-16 20:51:46 +0900

    refactor: 사용자에게 Exception 을 제대로 전달 할 수 있도록 수정

--- user-api/src/main/java/org/ecommerce/userapi/security/JwtFilter.java
+++ user-api/src/main/java/org/ecommerce/userapi/security/JwtFilter.java
@@ -2,12 +2,18 @@ package org.ecommerce.userapi.security;
 
 import java.io.IOException;
 
+import org.ecommerce.userapi.exception.UserErrorCode;
+import org.ecommerce.userapi.security.custom.ResponseConfigurer;
 import org.ecommerce.userapi.utils.RedisUtils;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +21,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class JwtFilter extends OncePerRequestFilter {
+public class JwtFilter extends OncePerRequestFilter implements ResponseConfigurer {
 
 	private final ProviderManager providerManager;
 	private final JwtUtils jwtUtils;
@@ -24,23 +30,32 @@ public class JwtFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
-
 		String jwt = jwtUtils.resolveToken(request);
 
-		if (jwt != null) {
-			boolean isLogout = redisUtils.hasKey(jwtUtils.getAccessTokenKey(jwtUtils.getUserId(jwt),jwtUtils.getAuthority(jwt)));
-			if (isLogout) {
-				// Authentication 검증 전
-				Authentication beforeAuthentication = jwtUtils.parseAuthentication(jwt);
-				// Authentication 검증 후
-				Authentication afterAuthenticate = providerManager.authenticate(beforeAuthentication);
-				// Context 에 저장
-				SecurityContextHolder.getContext().setAuthentication(afterAuthenticate);
-			}else {
-				response.getWriter().write("please ReLogin");
-				return;
+		try {
+			if (jwt != null) {
+				boolean isLogin = redisUtils.hasKey(
+					jwtUtils.getAccessTokenKey(jwtUtils.getUserId(jwt), jwtUtils.getAuthority(jwt)));
+				if (isLogin) {
+					// Authentication 검증 전
+					Authentication beforeAuthentication = jwtUtils.parseAuthentication(jwt);
+					// Authentication 검증 후
+					Authentication afterAuthenticate = providerManager.authenticate(beforeAuthentication);
+					// Context 에 저장
+					SecurityContextHolder.getContext().setAuthentication(afterAuthenticate);
+				}
 			}
+		} catch (ExpiredJwtException e) {
+			responseSetting(response, UserErrorCode.EXPIRED_JWT);
+			return;
+		} catch (UnsupportedJwtException | MalformedJwtException | SignatureException e) {
+			responseSetting(response, UserErrorCode.INVALID_SIGNATURE_JWT);
+			return;
+		} catch (IllegalArgumentException e) {
+			responseSetting(response, UserErrorCode.EMPTY_JWT);
+			return;
 		}
 		doFilter(request, response, filterChain);
 	}
 }
+
