4a62750aa275597481cbd9bc9607f4c32f4fb6cb
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-18 02:51:25 +0900

    Refactor : Dto 삭제 DTO 변경으로 인한 삭제

--- order-api/src/main/java/org/ecommerce/orderapi/vo/ResponseBucket.java
+++
@@ -1,9 +0,0 @@
-package org.ecommerce.orderapi.vo;
-
-public class ResponseBucket {
-	private Long id;
-	private Integer userId;
-	private String seller;
-	private Integer productId;
-	private Integer quantity;
-}
