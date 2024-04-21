f8202777b3b1977d5f59f65acc8de0e3a27bf813
no.oneho <oneho.dev@gmail.com>
2024-04-22 03:04:39 +0900

    Chore: queryDsl 재적용

--- product/src/main/java/org/ecommerce/product/config/QueryDslConfig.java
+++ product/src/main/java/org/ecommerce/product/config/QueryDslConfig.java
@@ -1,4 +1,4 @@
-package org.ecommerce.productsearchapi.config;
+package org.ecommerce.product.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
