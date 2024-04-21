92b7fb4da4a6fe6e61a360a490802ae746568aac
HongJongMin <gwa7417@gmail.com>
2024-04-21 20:57:22 +0900

    refactor: exclude DataSource 삭제

--- product/src/main/java/org/ecommerce/product/ProductApplication.java
+++ product/src/main/java/org/ecommerce/product/ProductApplication.java
@@ -2,12 +2,9 @@ package org.ecommerce.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//TODO 데이터베이스 연동 시 제거
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class ProductApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
 	}
