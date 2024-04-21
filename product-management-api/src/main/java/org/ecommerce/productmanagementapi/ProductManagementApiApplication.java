17978b495c767cb3db14f3f334a018f7444d3ef2
HongJongMin <gwa7417@gmail.com>
2024-04-21 23:44:12 +0900

    feat: EntityScan, EnableJpaRepository 추가

--- product-management-api/src/main/java/org/ecommerce/productmanagementapi/ProductManagementApiApplication.java
+++ product-management-api/src/main/java/org/ecommerce/productmanagementapi/ProductManagementApiApplication.java
@@ -2,14 +2,14 @@ package org.ecommerce.productmanagementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//TODO 데이터베이스 연동 시 제거
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"org.ecommerce.product.*","org.ecommerce.productmanagementapi","org.ecommerce.common"})
+@EntityScan("org.ecommerce.product.entity")
+@EnableJpaRepositories("org.ecommerce.productmanagementapi.repository")
 public class ProductManagementApiApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(ProductManagementApiApplication.class, args);
   }
-
 }
