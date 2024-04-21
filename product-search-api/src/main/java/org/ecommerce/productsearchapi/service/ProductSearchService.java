1095858bd962f5379930e99cc1a0ce789744bb6d
no.oneho <oneho.dev@gmail.com>
2024-04-22 03:05:59 +0900

    Refactor: 서비스코드 작성

--- product-search-api/src/main/java/org/ecommerce/productsearchapi/service/ProductSearchService.java
+++ product-search-api/src/main/java/org/ecommerce/productsearchapi/service/ProductSearchService.java
@@ -1,33 +1,30 @@
 package org.ecommerce.productsearchapi.service;
 
 import org.ecommerce.common.error.CustomException;
-import org.ecommerce.productsearchapi.dto.ProductMapper;
 import org.ecommerce.productsearchapi.dto.ProductSearchDto;
 import org.ecommerce.productsearchapi.exception.ProductSearchErrorCode;
-import org.ecommerce.productsearchapi.repository.jpa.ProductRepository;
+import org.ecommerce.productsearchapi.repository.querydsl.ProductCustomRepositoryImpl;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductSearchService {
 
-	private final ProductRepository productRepository;
+	private final ProductCustomRepositoryImpl productCustomRepository;
 
 	/**
 	 * @apiNote 노원호
-	 * name(상품명)을 통해 elasticsearch에 검색쿼리
+	 * product ID로 단일 레코드를 조회한다.
 	 * @param  productId 상품 ID
-	 * @return 테스트 과정이므로 별 다른 변환 과정 없이 Map 객체 반환
+	 * @return ProductSearchDto
 	 */
-	public ProductSearchDto getProductById(Integer productId) {
-
-		return ProductMapper.INSTANCE.toDto(productRepository.findById(productId).orElseThrow(
-				() -> new CustomException(ProductSearchErrorCode.NOT_FOUND_PRODUCT_ID)
-		));
-
+	public ProductSearchDto getProductById(final Integer productId) {
+		return productCustomRepository.findProductById(productId)
+			.orElseThrow(() -> new CustomException(ProductSearchErrorCode.NOT_FOUND_PRODUCT_ID));
 	}
-
 
 }
