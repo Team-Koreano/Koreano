721414623b805a11a1333b74ede86e2e3ec2498f
no.oneho <oneho.dev@gmail.com>
2024-04-22 03:05:51 +0900

    Refactor: 커스텀 에러 적용을 위한 common 패키지 주입

--- product-search-api/src/main/java/org/ecommerce/productsearchapi/ProductSearchApiApplication.java
+++ product-search-api/src/main/java/org/ecommerce/productsearchapi/ProductSearchApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"org.ecommerce.product","org.ecommerce.productsearchapi"})
+@SpringBootApplication(scanBasePackages = {"org.ecommerce.product","org.ecommerce.productsearchapi", "org.ecommerce.common"})
 @EntityScan(basePackages = {"org.ecommerce.product"})
 @EnableJpaRepositories(basePackages = {"org.ecommerce.productsearchapi"})
 public class ProductSearchApiApplication {
