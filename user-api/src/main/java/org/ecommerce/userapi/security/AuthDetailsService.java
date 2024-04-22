0c49a39e120b448c09c88a6134ca0ce1988218d5
HongJongMin <gwa7417@gmail.com>
2024-04-22 20:15:29 +0900

    refactor: 메서드 수정, 내부로직 수정

--- user-api/src/main/java/org/ecommerce/userapi/security/AuthDetailsService.java
+++ user-api/src/main/java/org/ecommerce/userapi/security/AuthDetailsService.java
@@ -12,7 +12,6 @@ import org.ecommerce.userapi.exception.UserErrorCode;
 import org.ecommerce.userapi.repository.SellerRepository;
 import org.ecommerce.userapi.repository.UserRepository;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -20,28 +19,27 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class AuthDetailsService implements UserDetailsService {
+public class AuthDetailsService {
 
 	private final UserRepository userRepository;
 	private final SellerRepository sellerRepository;
 
-	@Override
-	public AuthDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+	public AuthDetails getUserAuth(String email) throws UsernameNotFoundException {
 		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
-		// 일반 사용자 정보 가져오기
 		Optional<Users> user = userRepository.findByEmail(email);
 		if (user.isPresent()) {
-			// 권한 저장
 			authorities.add(new SimpleGrantedAuthority(Role.USER.getCode()));
-			// AuthDetails 의 필요한 정보를 저장
-			return new AuthDetails(user.get().getId(),user.get().getEmail(),authorities);
+			return new AuthDetails(user.get().getId(), user.get().getEmail(), authorities);
 		}
-		authorities = new HashSet<>();
-		// 판매자 정보 가져오기
+		throw new CustomException(UserErrorCode.NOT_FOUND_EMAIL);
+	}
+
+	public AuthDetails getSellerAuth(String email) throws UsernameNotFoundException {
+		Set<SimpleGrantedAuthority>	authorities = new HashSet<>();
 		Optional<Seller> seller = sellerRepository.findByEmail(email);
 		if (seller.isPresent()) {
 			authorities.add(new SimpleGrantedAuthority(Role.SELLER.getCode()));
-			return new AuthDetails(seller.get().getId(), seller.get().getEmail(),authorities);
+			return new AuthDetails(seller.get().getId(), seller.get().getEmail(), authorities);
 		}
 		throw new CustomException(UserErrorCode.NOT_FOUND_EMAIL);
 	}
