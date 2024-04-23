d8ad071832d4ee7bfbc662743a6cc1a0613e9089
HongJongMin <gwa7417@gmail.com>
2024-04-23 18:27:14 +0900

    fix: Conflict 해결

--- product-management-api/src/test/java/org/ecommerce/productmanagementapi/ProductManagementApiApplicationTests.java
+++ product-management-api/src/test/java/org/ecommerce/productmanagementapi/ProductManagementApiApplicationTests.java
@@ -2,8 +2,10 @@ package org.ecommerce.productmanagementapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = ProductManagementApiApplication.class)
+@ActiveProfiles("test")
 class ProductManagementApiApplicationTests {
 
   @Test
