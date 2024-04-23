2fbc072bc8f57980592e962a1376d7eaf46ddc58
HongJongMin <gwa7417@gmail.com>
2024-04-24 06:44:56 +0900

    refactor: BeanConfig 설정 변경

--- user-api/src/main/java/org/ecommerce/userapi/config/BeanConfiguration.java
+++ user-api/src/main/java/org/ecommerce/userapi/config/BeanConfiguration.java
@@ -1,14 +1,6 @@
 package org.ecommerce.userapi.config;
 
-import java.util.Collections;
-
-import javax.crypto.SecretKey;
-
-import org.ecommerce.userapi.security.AuthDetailsService;
-import org.ecommerce.userapi.security.JwtFilter;
 import org.ecommerce.userapi.security.JwtProvider;
-import org.ecommerce.userapi.security.JwtUtils;
-import org.ecommerce.userapi.utils.RedisUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ProviderManager;
@@ -20,31 +12,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BeanConfiguration {
 
-	private final RedisUtils redisUtils;
-	private final SecretKey secretKey;
-
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean
-	public JwtUtils jwtUtils() {
-			return new JwtUtils(redisUtils, secretKey);
-	}
-
-	@Bean
-	public JwtProvider jwtProvider(AuthDetailsService authDetailsService, JwtUtils jwtUtils) {
-		return new JwtProvider(authDetailsService, jwtUtils);
-	}
 
 	@Bean
 	public ProviderManager providerManager(JwtProvider jwtProvider) {
-		return new ProviderManager(Collections.singletonList(jwtProvider));
-	}
-
-	@Bean
-	public JwtFilter jwtFilter(ProviderManager providerManager, JwtUtils jwtUtils, RedisUtils redisUtils) {
-		return new JwtFilter(providerManager, jwtUtils, redisUtils);
+		return new ProviderManager(jwtProvider);
 	}
 }
