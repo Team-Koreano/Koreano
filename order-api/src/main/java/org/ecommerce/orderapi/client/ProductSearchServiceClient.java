6df5d11a68ad626b08a375e3c21a35e390cea062
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-23 23:23:14 +0900

    Refactor : valid Message 상수화

--- order-api/src/main/java/org/ecommerce/orderapi/client/ProductSearchServiceClient.java
+++ order-api/src/main/java/org/ecommerce/orderapi/client/ProductSearchServiceClient.java
@@ -10,7 +10,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "product-search-service", url = "${product-search-service.url}")
 public interface ProductSearchServiceClient {
 
-	// 장바구니에 담겨있는 상품 정보를 가져옴, 검증
+	/**
+	 * 재고를 확인하는 Internal API
+	 * <p>
+	 * 1. 상품의 유효성 검사를 합니다.
+	 * 2. 주문 상품의 수량과 현재 남아있는 재고량을 비교 검증을 합니다.
+	 * <p>
+	 * @author ${Juwon}
+	 *
+	 * @param productIds - 상품 번호가 들어간 리스트
+	 * @param quantities - 상품 수량이 들어간 리스트
+	 * @return ProductDto.Response - 재고를 확인한 상품의 정보
+	*/
 	@GetMapping
 	List<ProductDto.Response> checkStocks(
 			@RequestParam("productIds") final List<Integer> productIds,
