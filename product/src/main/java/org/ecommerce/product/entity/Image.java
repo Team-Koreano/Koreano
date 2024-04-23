198bd28ca88f8fd39ab0344ca23a9884e5e4291b
no.oneho <oneho.dev@gmail.com>
2024-04-23 21:47:10 +0900

    Refactor: 생성자 추가

--- product/src/main/java/org/ecommerce/product/entity/Image.java
+++ product/src/main/java/org/ecommerce/product/entity/Image.java
@@ -13,10 +13,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "image")
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class Image {
 
