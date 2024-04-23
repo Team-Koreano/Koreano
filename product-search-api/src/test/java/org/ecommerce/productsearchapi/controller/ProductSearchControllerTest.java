e0b221cbfc52f1306c6f0971683b79b8bfc80122
no.oneho <oneho.dev@gmail.com>
2024-04-23 23:34:41 +0900

    Test: Controller 테스트 작성

--- product-search-api/src/test/java/org/ecommerce/productsearchapi/controller/ProductSearchControllerTest.java
+++ product-search-api/src/test/java/org/ecommerce/productsearchapi/controller/ProductSearchControllerTest.java
@@ -8,6 +8,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.ecommerce.product.entity.Image;
+import org.ecommerce.product.entity.Product;
 import org.ecommerce.product.entity.SellerRep;
 import org.ecommerce.product.entity.type.Acidity;
 import org.ecommerce.product.entity.type.Bean;
@@ -39,8 +41,8 @@ public class ProductSearchControllerTest {
 		// given
 
 		final List<ImageDto> imageDtoList = List.of(
-			new ImageDto(1, true, (short)1, TEST_DATE_TIME, TEST_DATE_TIME, "http://image1.com"),
-			new ImageDto(2, false, (short)2, TEST_DATE_TIME, TEST_DATE_TIME, "http://image2.com")
+			new ImageDto(1, true, (short)1, TEST_DATE_TIME, TEST_DATE_TIME, "http://image1.com", false),
+			new ImageDto(2, false, (short)2, TEST_DATE_TIME, TEST_DATE_TIME, "http://image2.com", false)
 		);
 
 		final ProductSearchDto productSearchDto =
@@ -80,27 +82,53 @@ public class ProductSearchControllerTest {
 			.andExpect(jsonPath("$.result.information").value(productSearchDto.getInformation()))
 			.andExpect(jsonPath("$.result.status").value(productSearchDto.getStatus().getTitle()))
 			.andExpect(jsonPath("$.result.isCrush").value(productSearchDto.getIsCrush()))
-			.andExpect(jsonPath("$.result.createDateTime").value(productSearchDto.getCreateDateTime().toString()))
+			.andExpect(jsonPath("$.result.createDatetime").value(productSearchDto.getCreateDatetime().toString()))
 			.andExpect(jsonPath("$.result.imageDtoList[0].id").value(imageDtoList.get(0).getId()))
 			.andExpect(jsonPath("$.result.imageDtoList[0].isThumbnail").value(imageDtoList.get(0).getIsThumbnail()))
 			.andExpect(jsonPath("$.result.imageDtoList[0].sequenceNumber").value(
 				Integer.valueOf(imageDtoList.get(0).getSequenceNumber())))
-			.andExpect(jsonPath("$.result.imageDtoList[0].createDateTime").value(
-				imageDtoList.get(0).getCreateDateTime().toString()))
-			.andExpect(jsonPath("$.result.imageDtoList[0].updateDateTime").value(
-				imageDtoList.get(0).getUpdateDateTime().toString()))
+			.andExpect(jsonPath("$.result.imageDtoList[0].createDatetime").value(
+				imageDtoList.get(0).getCreateDatetime().toString()))
+			.andExpect(jsonPath("$.result.imageDtoList[0].updateDatetime").value(
+				imageDtoList.get(0).getUpdateDatetime().toString()))
 			.andExpect(jsonPath("$.result.imageDtoList[0].imageUrl").value(imageDtoList.get(0).getImageUrl()))
 			.andExpect(jsonPath("$.result.imageDtoList[1].id").value(imageDtoList.get(1).getId()))
 			.andExpect(jsonPath("$.result.imageDtoList[1].isThumbnail").value(imageDtoList.get(1).getIsThumbnail()))
 			.andExpect(jsonPath("$.result.imageDtoList[1].sequenceNumber").value(
 				Integer.valueOf(imageDtoList.get(1).getSequenceNumber())))
-			.andExpect(jsonPath("$.result.imageDtoList[1].createDateTime").value(
-				imageDtoList.get(1).getCreateDateTime().toString()))
-			.andExpect(jsonPath("$.result.imageDtoList[1].updateDateTime").value(
-				imageDtoList.get(1).getUpdateDateTime().toString()))
+			.andExpect(jsonPath("$.result.imageDtoList[1].createDatetime").value(
+				imageDtoList.get(1).getCreateDatetime().toString()))
+			.andExpect(jsonPath("$.result.imageDtoList[1].updateDatetime").value(
+				imageDtoList.get(1).getUpdateDatetime().toString()))
 			.andExpect(jsonPath("$.result.imageDtoList[1].imageUrl").value(imageDtoList.get(1).getImageUrl()))
 			.andExpect(status().isOk())
 			.andDo(print());
+	}
+
+	private Product getProduct() {
+		final List<Image> images = List.of(
+			new Image(1, null, "http://image1.com", true, (short)1, false, TEST_DATE_TIME, TEST_DATE_TIME),
+			new Image(2, null, "http://image2.com", false, (short)2, false, TEST_DATE_TIME, TEST_DATE_TIME)
+		);
+
+		return new Product(
+			1,
+			ProductCategory.BEAN,
+			30000,
+			100,
+			new SellerRep(1, "커피천국"),
+			10,
+			false,
+			"[특가 EVENT]&아라비카 원두&세상에서 제일 존맛 커피",
+			Bean.ARABICA,
+			Acidity.MEDIUM,
+			"커피천국에서만 만나볼 수 있는 특별한 커피",
+			false,
+			ProductStatus.AVAILABLE,
+			TEST_DATE_TIME,
+			TEST_DATE_TIME,
+			images
+		);
 	}
 
 }
