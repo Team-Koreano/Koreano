41339fd54122a61fa93e504c4ec6736ab9230afd
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-25 09:16:38 +0900

    Feat : Redis Test용 API 구현

--- order-api/src/main/java/org/ecommerce/orderapi/entity/Stock.java
+++ order-api/src/main/java/org/ecommerce/orderapi/entity/Stock.java
@@ -1,21 +1,18 @@
 package org.ecommerce.orderapi.entity;
 
-import org.springframework.data.redis.core.RedisHash;
-
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
-@RedisHash(value = "stock")
 public class Stock {
 
-	@Id
 	private Integer productId;
 
-	private Integer totalStock;
-	private Integer processingStock;
+	private Integer total;
+	private Integer processingCnt;
+
+	public Integer getAvailableStock() {
+		return this.total - this.processingCnt;
+	}
 }
