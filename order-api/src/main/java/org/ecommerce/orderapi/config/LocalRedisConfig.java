93f7f932caf46d2b58a6b9731c061c8827496edc
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-25 11:10:55 +0900

    Chore : 의존성 추가 및 profile 분리 Embedded Redis 추가

---
+++ order-api/src/main/java/org/ecommerce/orderapi/config/LocalRedisConfig.java
@@ -0,0 +1,39 @@
+package org.ecommerce.orderapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import redis.embedded.RedisServer;
+
+@Profile("dev")
+@Slf4j
+@Configuration
+public class LocalRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() {
+        try {
+            redisServer = new RedisServer(redisPort);
+            redisServer.start();
+        } catch (Exception e) {
+            // 예외 처리
+            log.info("빌드 FAILED 해결을 위한 Embedded Redis");
+        }
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
