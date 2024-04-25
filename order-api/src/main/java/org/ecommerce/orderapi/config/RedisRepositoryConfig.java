77bcf44aaaff2196bdf0fd0292b9ee14ce459211
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-04-25 22:35:06 +0900

    Fix : RedisRepositoryConfig 수정

--- order-api/src/main/java/org/ecommerce/orderapi/config/RedisRepositoryConfig.java
+++ order-api/src/main/java/org/ecommerce/orderapi/config/RedisRepositoryConfig.java
@@ -6,10 +6,11 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
+@Profile({"local"})
 @Configuration
 public class RedisRepositoryConfig {
 
