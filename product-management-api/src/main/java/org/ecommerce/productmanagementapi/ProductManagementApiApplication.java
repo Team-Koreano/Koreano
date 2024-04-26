2c43f6991bb56d71c914eb142079ab0ef84e4183
HongJongMin <gwa7417@gmail.com>
2024-04-27 00:26:47 +0900

    feat: ScanConfig  추가

--- product-management-api/src/main/java/org/ecommerce/productmanagementapi/ProductManagementApiApplication.java
+++ product-management-api/src/main/java/org/ecommerce/productmanagementapi/ProductManagementApiApplication.java
@@ -2,12 +2,8 @@ package org.ecommerce.productmanagementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"org.ecommerce.product.*","org.ecommerce.productmanagementapi","org.ecommerce.common"})
-@EntityScan("org.ecommerce.product.entity")
-@EnableJpaRepositories("org.ecommerce.productmanagementapi.repository")
 public class ProductManagementApiApplication {
 
   public static void main(String[] args) {
