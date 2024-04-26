4e8e82f428a1701a4205be8cd3b30e2699006b00
HongJongMin <gwa7417@gmail.com>
2024-04-27 00:25:52 +0900

    feat: 상품수정, 재고수정, 상태값수정 구현

---
+++ product-management-api/src/main/java/org/ecommerce/productmanagementapi/controller/ExternalProductManagementController.java
@@ -0,0 +1,60 @@
+package org.ecommerce.productmanagementapi.controller;
+
+import org.ecommerce.common.vo.Response;
+import org.ecommerce.product.entity.type.ProductStatus;
+import org.ecommerce.productmanagementapi.dto.ProductManagementDto;
+import org.ecommerce.productmanagementapi.dto.ProductManagementMapper;
+import org.ecommerce.productmanagementapi.service.ProductManagementService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/external/productmanagement/v1")
+@RequiredArgsConstructor
+public class ExternalProductManagementController {
+
+	private final ProductManagementService productManagementService;
+	// TODO : S3를 통한 이미지 저장 로직 추가
+	// TODO : 상품 UD, 예외 사항 추가,
+
+	@PostMapping()
+	public Response<ProductManagementDto.Response> register(
+		@Valid @RequestBody ProductManagementDto.Request.Register product) {
+		ProductManagementDto productManagementDto = productManagementService.productRegister(product);
+		return new Response<>(HttpStatus.OK.value(), ProductManagementMapper.INSTANCE.toResponse(productManagementDto));
+	}
+
+	@PutMapping("/status/{productId}/{status}")
+	public Response<ProductManagementDto.Response> modifyToStatus(
+		@PathVariable("productId") final Integer productId,
+		@PathVariable("status") final ProductStatus status
+	) {
+		ProductManagementDto productManagementDto = productManagementService.modifyToStatus(productId, status);
+		return new Response<>(HttpStatus.OK.value(), ProductManagementMapper.INSTANCE.toResponse(productManagementDto));
+	}
+
+	@PutMapping("/stock")
+	public Response<ProductManagementDto.Response> modifyToStock(
+		@RequestBody final ProductManagementDto.Request.Stock stock
+	) {
+		ProductManagementDto productManagementDto = productManagementService.modifyToStock(stock);
+		return new Response<>(HttpStatus.OK.value(), ProductManagementMapper.INSTANCE.toResponse(productManagementDto));
+	}
+
+	@PutMapping("/{productId}")
+	public Response<ProductManagementDto.Response> modifyToProduct(
+		@PathVariable("productId") final Integer productId,
+		@RequestBody final ProductManagementDto.Request.Modify modifyProduct
+	) {
+		ProductManagementDto productManagementDto = productManagementService.modifyToProduct(productId, modifyProduct);
+		return new Response<>(HttpStatus.OK.value(), ProductManagementMapper.INSTANCE.toResponse(productManagementDto));
+	}
+}
