0c36577a550a6f1fa658ec051215e6ecc3d39ecb
no.oneho <oneho.dev@gmail.com>
2024-04-27 19:06:25 +0900

    Feat: Product Document 작성

--- product-search-api/src/main/java/org/ecommerce/productsearchapi/config/ElasticSearchConfig.java
+++ product-search-api/src/main/java/org/ecommerce/productsearchapi/config/ElasticSearchConfig.java
@@ -1,16 +1,14 @@
 package org.ecommerce.productsearchapi.config;
 
+import org.ecommerce.productsearchapi.repository.ProductElasticsearchRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 
-@EnableElasticsearchRepositories
+@EnableElasticsearchRepositories(basePackageClasses = ProductElasticsearchRepository.class)
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
-
-	@Value("${spring.elasticsearch.uris}")
-	private String uris;
 
 	@Value("${spring.elasticsearch.username}")
 	private String username;
@@ -18,10 +16,12 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
 	@Value("${spring.elasticsearch.password}")
 	private String password;
 
+	//TODO 추후 url yml 이나 환경변수로 관리 예정
+
 	@Override
 	public ClientConfiguration clientConfiguration() {
 		return ClientConfiguration.builder()
-				.connectedTo(uris)
+				.connectedTo("localhost:9200")
 				.withBasicAuth(username, password)
 				.build();
 	}
