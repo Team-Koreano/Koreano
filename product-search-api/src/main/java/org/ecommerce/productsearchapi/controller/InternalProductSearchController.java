f12c4abdb918c58e4ab6bb056094805c8993754a
no.oneho <oneho.dev@gmail.com>
2024-04-28 20:59:09 +0900

    Refactor: Request Body 어노테이션 추가

--- product-search-api/src/main/java/org/ecommerce/productsearchapi/controller/InternalProductSearchController.java
+++ product-search-api/src/main/java/org/ecommerce/productsearchapi/controller/InternalProductSearchController.java
@@ -4,6 +4,7 @@ import org.ecommerce.product.entity.Product;
 import org.ecommerce.productsearchapi.dto.ProductSearchDto;
 import org.ecommerce.productsearchapi.service.ProductSearchService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class InternalProductSearchController {
 	private final ProductSearchService productSearchService;
 
 	@PostMapping
-	public ProductSearchDto.Response.SavedProduct saveRecord(Product product) {
+	public ProductSearchDto.Response.SavedProduct saveRecord(@RequestBody Product product) {
 		return ProductSearchDto.Response.SavedProduct.of(productSearchService.saveProduct(product));
 	}
 
