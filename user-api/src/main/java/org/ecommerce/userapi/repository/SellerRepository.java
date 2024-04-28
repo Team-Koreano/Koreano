7886ec9562fbf88330cadfea529e484f4c7833f8
HongJongMin <gwa7417@gmail.com>
2024-04-28 18:47:58 +0900

    refactor: repository 수정

--- user-api/src/main/java/org/ecommerce/userapi/repository/SellerRepository.java
+++ user-api/src/main/java/org/ecommerce/userapi/repository/SellerRepository.java
@@ -5,12 +5,13 @@ import java.util.Optional;
 import org.ecommerce.userapi.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface SellerRepository extends JpaRepository<Seller, Integer> {
-	boolean existsByEmailOrPhoneNumber(String email,String phoneNumber);
+	@Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
+	boolean existsByEmailOrPhoneNumber(String email, String phoneNumber);
 
 	Optional<Seller> findByEmail(String email);
-
-	Optional<Seller> findSellerById(Integer id);
 }
