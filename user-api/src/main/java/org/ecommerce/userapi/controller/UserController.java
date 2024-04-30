118b595b95be6e27677b5517d46662b5fca33b9a
HongJongMin <gwa7417@gmail.com>
2024-05-01 02:23:58 +0900

    feat: reissue 로직 추가

--- user-api/src/main/java/org/ecommerce/userapi/controller/UserController.java
+++ user-api/src/main/java/org/ecommerce/userapi/controller/UserController.java
@@ -5,14 +5,17 @@ import org.ecommerce.userapi.dto.AccountDto;
 import org.ecommerce.userapi.dto.AddressDto;
 import org.ecommerce.userapi.dto.UserDto;
 import org.ecommerce.userapi.security.AuthDetails;
+import org.ecommerce.userapi.security.custom.CurrentUser;
 import org.ecommerce.userapi.service.UserService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +26,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/users/v1")
 public class UserController {
 
-
 	private final UserService userService;
 
 	@PostMapping()
@@ -33,20 +35,23 @@ public class UserController {
 	}
 
 	@PostMapping("/login")
-	public Response<UserDto.Response.Login> login(@RequestBody final UserDto.Request.Login login) {
-		UserDto userDto = userService.loginRequest(login);
+	public Response<UserDto.Response.Login> login(
+		@RequestBody final UserDto.Request.Login login,
+		HttpServletResponse response
+	) {
+		UserDto userDto = userService.loginRequest(login, response);
 		return new Response<>(HttpStatus.OK.value(), UserDto.Response.Login.of(userDto));
 	}
 
 	@PostMapping("/logout")
-	public Response<String> logout(@AuthenticationPrincipal final AuthDetails authDetails) {
+	public Response<String> logout(@CurrentUser final AuthDetails authDetails) {
 		userService.logoutRequest(authDetails);
 		return new Response<>(HttpStatus.OK.value(), "로그아웃에 성공하였습니다");
 	}
 
 	@PostMapping("/address")
 	public Response<AddressDto.Response.Register> address(
-		@AuthenticationPrincipal final AuthDetails authDetails,
+		@CurrentUser final AuthDetails authDetails,
 		@RequestBody @Valid final AddressDto.Request.Register address) {
 		final AddressDto addressDto = userService.registerAddress(authDetails, address);
 		return new Response<>(HttpStatus.OK.value(), AddressDto.Response.Register.of(addressDto));
@@ -54,9 +59,18 @@ public class UserController {
 
 	@PostMapping("/account")
 	public Response<AccountDto.Response.Register> account(
-		@AuthenticationPrincipal final AuthDetails authDetails,
+		@CurrentUser final AuthDetails authDetails,
 		@RequestBody @Valid final AccountDto.Request.Register account) {
 		AccountDto accountDto = userService.registerAccount(authDetails, account);
 		return new Response<>(HttpStatus.OK.value(), AccountDto.Response.Register.of(accountDto));
+	}
+
+	@PostMapping("/reissue")
+	public Response<UserDto.Response.Login> reissueAccessToken(
+		@RequestHeader(HttpHeaders.AUTHORIZATION) final String bearerToken,
+		HttpServletResponse response
+	) {
+		UserDto userDto = userService.reissueAccessToken(bearerToken, response);
+		return new Response<>(HttpStatus.OK.value(), UserDto.Response.Login.of(userDto));
 	}
 }
