25644a12839acad4f07379a0e0e996fcfa7d4940
HongJongMin <gwa7417@gmail.com>
2024-05-01 02:57:24 +0900

    refactor: Repository 메서드 수정

--- user-api/src/main/java/org/ecommerce/userapi/repository/UserRepository.java
+++ user-api/src/main/java/org/ecommerce/userapi/repository/UserRepository.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 import org.ecommerce.userapi.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
-	boolean existsByEmailOrPhoneNumber(String email,String phoneNumber);
+	@Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
+	boolean existsByEmailOrPhoneNumber(String email, String phoneNumber);
+
 	Optional<Users> findUsersByEmail(String email);
 
-	Optional<Users> findUsersById(Integer id);
 }
