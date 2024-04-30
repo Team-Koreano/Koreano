da4656ebc6bb6fc5e71e02fd0a36f033c1a3d01d
HongJongMin <gwa7417@gmail.com>
2024-05-01 02:57:14 +0900

    refactor: 엑세스토큰 재발급 로직 추가

--- user-api/src/main/java/org/ecommerce/userapi/controller/SellerController.java
+++ user-api/src/main/java/org/ecommerce/userapi/controller/SellerController.java
@@ -4,14 +4,17 @@ import org.ecommerce.common.vo.Response;
 import org.ecommerce.userapi.dto.AccountDto;
 import org.ecommerce.userapi.dto.SellerDto;
 import org.ecommerce.userapi.security.AuthDetails;
+import org.ecommerce.userapi.security.custom.CurrentUser;
 import org.ecommerce.userapi.service.SellerService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,26 +32,39 @@ public class SellerController {
 
 	@PostMapping()
 	public Response<SellerDto.Response.Register> register(@RequestBody final SellerDto.Request.Register seller) {
-		final SellerDto responseSeller =  sellerService.registerRequest(seller);
+		final SellerDto responseSeller = sellerService.registerRequest(seller);
 		return new Response<>(HttpStatus.OK.value(), SellerDto.Response.Register.of(responseSeller));
 	}
+
 	@PostMapping("/login")
-	public Response<SellerDto.Response.Login> login(@RequestBody final SellerDto.Request.Login login) {
-		final SellerDto responseLogin = sellerService.loginRequest(login);
-		return new Response<>(HttpStatus.OK.value(),SellerDto.Response.Login.of(responseLogin));
+	public Response<SellerDto.Response.Login> login(
+		@RequestBody final SellerDto.Request.Login login,
+		HttpServletResponse response
+	) {
+		final SellerDto responseLogin = sellerService.loginRequest(login, response);
+		return new Response<>(HttpStatus.OK.value(), SellerDto.Response.Login.of(responseLogin));
 	}
+
 	@PostMapping("/logout")
-	public Response<String> logout(@AuthenticationPrincipal final AuthDetails authDetails){
+	public Response<String> logout(@CurrentUser final AuthDetails authDetails) {
 		sellerService.logoutRequest(authDetails);
 		return new Response<>(HttpStatus.OK.value(), "로그아웃에 성공하였습니다");
 	}
 
 	@PostMapping("/account")
 	public Response<AccountDto.Response.Register> account(
-		@AuthenticationPrincipal final AuthDetails authDetails,
-		@RequestBody @Valid final AccountDto.Request.Register account){
+		@CurrentUser final AuthDetails authDetails,
+		@RequestBody @Valid final AccountDto.Request.Register account) {
 		AccountDto accountDto = sellerService.registerAccount(authDetails, account);
-		return new Response<>(HttpStatus.OK.value(),AccountDto.Response.Register.of(accountDto));
+		return new Response<>(HttpStatus.OK.value(), AccountDto.Response.Register.of(accountDto));
+	}
 
+	@PostMapping("/reissue")
+	public Response<SellerDto.Response.Login> reissueAccessToken(
+		@RequestHeader(HttpHeaders.AUTHORIZATION) final String bearerToken,
+		HttpServletResponse response
+	) {
+		SellerDto sellerDto = sellerService.reissueAccessToken(bearerToken, response);
+		return new Response<>(HttpStatus.OK.value(), SellerDto.Response.Login.of(sellerDto));
 	}
 }
