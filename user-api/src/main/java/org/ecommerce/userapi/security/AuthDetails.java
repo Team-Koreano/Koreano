81c9581ff1da087049e47cf0277a7c9b107403a0
HongJongMin <gwa7417@gmail.com>
2024-05-01 02:55:57 +0900

    refactor: AuthDetails 수정

--- user-api/src/main/java/org/ecommerce/userapi/security/AuthDetails.java
+++ user-api/src/main/java/org/ecommerce/userapi/security/AuthDetails.java
@@ -1,54 +1,12 @@
 package org.ecommerce.userapi.security;
 
-import java.util.Collection;
-import java.util.Set;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
-public class AuthDetails implements UserDetails {
-	private Integer userId;
+@AllArgsConstructor
+public class AuthDetails {
+	private Integer id;
 	private String email;
-	private Set<SimpleGrantedAuthority> authoritySet;
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return authoritySet;
-	}
-
-	@Override
-	public String getPassword() {
-		return null;
-	}
-
-	@Override
-	public String getUsername() {
-		return null;
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		return false;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return false;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return false;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return false;
-	}
+	private String roll;
 }
