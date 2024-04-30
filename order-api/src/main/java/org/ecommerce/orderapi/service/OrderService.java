ed7b30ff215c96c3c3799b6aa8ec71a220a0bb84
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-05-01 05:26:55 +0900

    Refactor : loop 최소화 #112

--- order-api/src/main/java/org/ecommerce/orderapi/service/OrderService.java
+++ order-api/src/main/java/org/ecommerce/orderapi/service/OrderService.java
@@ -1,26 +1,21 @@
 package org.ecommerce.orderapi.service;
 
-import static org.ecommerce.orderapi.dto.BucketDto.*;
 import static org.ecommerce.orderapi.exception.OrderErrorCode.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 import org.ecommerce.common.error.CustomException;
 import org.ecommerce.orderapi.client.BucketServiceClient;
 import org.ecommerce.orderapi.client.RedisClient;
-import org.ecommerce.orderapi.dto.BucketDto;
 import org.ecommerce.orderapi.dto.BucketMapper;
+import org.ecommerce.orderapi.dto.BucketSummary;
 import org.ecommerce.orderapi.dto.OrderDto;
 import org.ecommerce.orderapi.dto.OrderMapper;
 import org.ecommerce.orderapi.entity.Order;
-import org.ecommerce.orderapi.entity.OrderDetail;
-import org.ecommerce.orderapi.entity.OrderStatusHistory;
 import org.ecommerce.orderapi.entity.Product;
 import org.ecommerce.orderapi.entity.Stock;
-import org.ecommerce.orderapi.entity.type.OrderStatus;
-import org.ecommerce.orderapi.entity.type.ProductStatus;
+import org.ecommerce.orderapi.entity.enumerated.ProductStatus;
 import org.ecommerce.orderapi.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,9 +33,6 @@ public class OrderService {
 	private final BucketServiceClient bucketServiceClient;
 	private final OrderRepository orderRepository;
 	private final RedisClient redisClient;
-	private final StockService stockService;
-
-	private final static Integer DELIVERY_FEE = 0;
 
 	// TODO user-service 검증 : user-service 구축 이후
 	// TODO bucket-service 상품 가져오기, 장바구니 검증
@@ -57,14 +49,14 @@ public class OrderService {
 	 * @return - 장바구니 정보가 들어있는 BucketDto 입니다.
 	 */
 	@VisibleForTesting
-	public List<BucketDto> getBuckets(
+	public BucketSummary getBuckets(
 			final Integer userId,
 			final List<Long> bucketIds
 	) {
-		return bucketServiceClient.getBuckets(userId, bucketIds)
-				.stream()
-				.map(BucketMapper.INSTANCE::responseToDto)
-				.toList();
+		return BucketSummary.ofCreate(
+				bucketServiceClient.getBuckets(userId, bucketIds)
+						.stream()
+						.map(BucketMapper.INSTANCE::responseToDto).toList());
 	}
 
 	/**
@@ -125,85 +117,29 @@ public class OrderService {
 			final Integer userId,
 			final OrderDto.Request.Place request
 	) {
-		final List<BucketDto> bucketDtos = getBuckets(userId, request.bucketIds());
-		final List<Integer> productIds = toProductIds(bucketDtos);
-		final Map<Integer, Integer> productIdToQuantityMap = toProductIdToQuantityMap(bucketDtos);
+		final BucketSummary bucketSummary = getBuckets(userId, request.bucketIds());
+		final List<Integer> productIds = bucketSummary.getProductIds();
+		final Map<Integer, Integer> productIdToQuantityMap = bucketSummary.getProductIdToQuantityMap();
+
+		// TODO : FeignClient Product 정보 받기
 		final List<Product> products = redisClient.getProducts(productIds);
 
 		validateStock(productIds, productIdToQuantityMap);
 		validateProduct(products);
 
-		Order order = Order.ofPlace(
-				userId,
-				request.receiveName(),
-				request.phoneNumber(),
-				request.address1(),
-				request.address2(),
-				request.deliveryComment()
+		return OrderMapper.INSTANCE.toDto(
+				orderRepository.save(
+						Order.ofPlace(
+								userId,
+								request.receiveName(),
+								request.phoneNumber(),
+								request.address1(),
+								request.address2(),
+								request.deliveryComment(),
+								products,
+								productIdToQuantityMap
+						)
+				)
 		);
-
-		List<OrderDetail> orderDetails = placeOrderDetails(
-				order,
-				productIdToQuantityMap,
-				products
-		);
-
-		recordOrderStatusHistory(orderDetails);
-		order.attachOrderDetails(orderDetails);
-		orderRepository.save(order);
-		stockService.increaseInProcessingStocks(orderDetails);
-		return OrderMapper.INSTANCE.toDto(order);
-	}
-
-	// TODO : 배송비 우선 무료로 고정, 추후 seller에서 정책 설정
-
-	/**
-	 * 주문 상세를 생성하는 메소드입니다.
-	 * @author ${Juwon}
-	 *
-	 * @param order- 주문 정보
-	 * @param productIdToQuantityMap- Map, key:productId, value:quantity
-	 * @param products- 상품 정보
-	 * @return List<OrderDetail>- 주문 상세 리스트
-	 */
-	@VisibleForTesting
-	public List<OrderDetail> placeOrderDetails(
-			final Order order,
-			final Map<Integer, Integer> productIdToQuantityMap,
-			final List<Product> products
-	) {
-		return products.stream()
-				.map(product -> {
-					Integer productId = product.getId();
-					return OrderDetail.ofPlace(
-							order,
-							product.getId(),
-							product.getPrice(),
-							productIdToQuantityMap.get(productId),
-							DELIVERY_FEE,
-							product.getSeller()
-					);
-				})
-				.toList();
-	}
-
-	/**
-	 * 주문 상세에 대한 상태 이력을 기록하는 메소드입니다.
-	 * @author ${Juwon}
-	 *
-	 * @param orderDetails - 주문 상세 리스트
-	*/
-	@VisibleForTesting
-	public void recordOrderStatusHistory(final List<OrderDetail> orderDetails) {
-		List<OrderStatusHistory> orderStatusHistories = orderDetails.stream()
-				.map(orderDetail -> OrderStatusHistory.ofRecord(
-						orderDetail,
-						OrderStatus.OPEN
-				))
-				.toList();
-
-		IntStream.range(0, orderDetails.size())
-				.forEach(i -> orderDetails.get(i)
-						.recordOrderStatusHistory(orderStatusHistories.get(i)));
 	}
 }
