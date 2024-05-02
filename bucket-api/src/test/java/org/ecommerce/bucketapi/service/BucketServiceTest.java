fe89b89d547ff84fec2058cb63078c7f9b95609c
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-05-02 21:26:32 +0900

    Refactor : TestCode 수정 BucketService 수정 적용

--- bucket-api/src/test/java/org/ecommerce/bucketapi/service/BucketServiceTest.java
+++ bucket-api/src/test/java/org/ecommerce/bucketapi/service/BucketServiceTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,14 +29,24 @@ public class BucketServiceTest {
 	@Mock
 	private BucketRepository bucketRepository;
 
-	private static final LocalDate CREATE_DATE = LocalDate.now();
-
 	@Test
 	void 장바구니_조회() {
 		// given
 		List<Bucket> buckets = List.of(
-				new Bucket(1L, 1, "seller1", 101, 3, CREATE_DATE),
-				new Bucket(2L, 1, "seller2", 102, 2, CREATE_DATE)
+				new Bucket(1L,
+						1,
+						"seller1",
+						101,
+						3,
+						LocalDate.of(2024, 5, 2)
+				),
+				new Bucket(2L,
+						1,
+						"seller2",
+						102,
+						2,
+						LocalDate.of(2024, 5, 2)
+				)
 		);
 		given(bucketRepository.findAllByUserId(anyInt()))
 				.willReturn(buckets);
@@ -64,14 +75,14 @@ public class BucketServiceTest {
 						103,
 						1
 				);
-		Bucket savedBucket = new Bucket(
-				1L,
+		Bucket savedBucket = new Bucket(1L,
 				1,
-				"returnSellerName",
+				"seller1",
 				101,
 				3,
-				CREATE_DATE
+				LocalDate.of(2024, 5, 2)
 		);
+
 		given(bucketRepository.save(any(Bucket.class)))
 				.willReturn(savedBucket);
 		final ArgumentCaptor<Bucket> captor = ArgumentCaptor.forClass(Bucket.class);
@@ -96,27 +107,29 @@ public class BucketServiceTest {
 	void 장바구니_수정() {
 		// given
 		final Integer newQuantity = 777;
-		given(bucketRepository.findById(anyLong()))
-				.willReturn(Optional.of(
-								new Bucket(
-										1L,
-										1,
-										"seller",
-										101,
-										3,
-										CREATE_DATE
-								)
-						)
-				);
+		final Bucket savedBucket = spy(
+				new Bucket(1L,
+						1,
+						"seller1",
+						101,
+						3,
+						LocalDate.of(2024, 5, 2)
+				)
+		);
+		final BucketDto.Request.Modify request = new BucketDto
+				.Request.Modify(newQuantity);
+		given(bucketRepository.findByIdAndUserId(anyLong(), anyInt()))
+				.willReturn(Optional.of(savedBucket));
 
 		// when
 		final BucketDto bucketDto = bucketService.modifyBucket(
 				1,
 				1L,
-				new BucketDto.Request.Modify(newQuantity)
+				request
 		);
 
 		// then
+		verify(savedBucket, Mockito.times(1)).modifyQuantity(request.quantity());
 		assertEquals(newQuantity, bucketDto.getQuantity());
 	}
 
@@ -125,7 +138,7 @@ public class BucketServiceTest {
 		// given
 		final BucketDto.Request.Modify bucketModifyRequest =
 				new BucketDto.Request.Modify(777);
-		given(bucketRepository.findById(anyLong()))
+		given(bucketRepository.findByIdAndUserId(anyLong(), anyInt()))
 				.willReturn(Optional.empty());
 
 		// when
@@ -137,39 +150,27 @@ public class BucketServiceTest {
 	}
 
 	@Test
-	void 회원에게_유효하지_않은_장바구니를_수정할_때() {
-		// given
-		final BucketDto.Request.Modify bucketModifyRequest =
-				new BucketDto.Request.Modify(777);
-		Bucket bucket = new Bucket(
-				2L,
-				2,
-				"seller2",
-				102,
-				3,
-				CREATE_DATE
-		);
-		given(bucketRepository.findById(anyLong()))
-				.willReturn(Optional.of(bucket));
-
-		// when
-		CustomException exception = assertThrows(CustomException.class,
-				() -> bucketService.modifyBucket(1, 1L, bucketModifyRequest));
-
-		// then
-		assertEquals(BucketErrorCode.INVALID_BUCKET_WITH_USER, exception.getErrorCode());
-	}
-
-	@Test
 	void 회원이_선택한_장바구니_조회() {
 		// given
 		final Integer userId = 1;
 		final List<Long> bucketIds = List.of(1L, 2L);
 		final List<Bucket> buckets = List.of(
-				new Bucket(1L, 1, "seller1", 101, 3, CREATE_DATE),
-				new Bucket(2L, 1, "seller2", 102, 2, CREATE_DATE)
+				new Bucket(1L,
+						1,
+						"seller1",
+						101,
+						3,
+						LocalDate.of(2024, 5, 2)
+				),
+				new Bucket(2L,
+						1,
+						"seller2",
+						102,
+						2,
+						LocalDate.of(2024, 5, 2)
+				)
 		);
-		given(bucketRepository.findAllById(bucketIds))
+		given(bucketRepository.findAllByIdInAndUserId(bucketIds, userId))
 				.willReturn(buckets);
 
 		// when
@@ -189,20 +190,32 @@ public class BucketServiceTest {
 
 	@Test
 	void 회원이_선택한_장바구니_누락_조회() {
-	    // given
+		// given
 		final Integer userId = 1;
 		final List<Long> bucketIds = List.of(1L, 2L, 3L);
 		final List<Bucket> buckets = List.of(
-				new Bucket(1L, 1, "seller1", 101, 3, CREATE_DATE),
-				new Bucket(2L, 1, "seller2", 102, 2, CREATE_DATE)
+				new Bucket(1L,
+						1,
+						"seller1",
+						101,
+						3,
+						LocalDate.of(2024, 5, 2)
+				),
+				new Bucket(2L,
+						1,
+						"seller2",
+						102,
+						2,
+						LocalDate.of(2024, 5, 2)
+				)
 		);
-		given(bucketRepository.findAllById(bucketIds))
+		given(bucketRepository.findAllByIdInAndUserId(anyList(), anyInt()))
 				.willReturn(buckets);
-	    // when
+		// when
 		CustomException exception = assertThrows(CustomException.class,
 				() -> bucketService.getBuckets(userId, bucketIds));
 
-	    // then
+		// then
 		assertEquals(BucketErrorCode.NOT_FOUND_BUCKET_ID, exception.getErrorCode());
 	}
 }
