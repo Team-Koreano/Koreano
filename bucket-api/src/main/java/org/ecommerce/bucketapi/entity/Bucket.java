998cf3c8893ad43bcdd48221065bad4a266eef0e
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-05-02 21:24:58 +0900

    Refactor : BucketEntity 수정 Annotation 삭제

--- bucket-api/src/main/java/org/ecommerce/bucketapi/entity/Bucket.java
+++ bucket-api/src/main/java/org/ecommerce/bucketapi/entity/Bucket.java
@@ -4,20 +4,21 @@ import java.time.LocalDate;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "bucket")
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Bucket {
 
@@ -41,18 +42,37 @@ public class Bucket {
 	@Column
 	private LocalDate createDate;
 
-	public static Bucket ofAdd(
-		final Integer userId,
-		final String seller,
-		final Integer productId,
-		final Integer quantity
+	@VisibleForTesting
+	public Bucket(
+			final Long id,
+			final Integer userId,
+			final String seller,
+			final Integer productId,
+			final Integer quantity,
+			final LocalDate createDate
 	) {
-		Bucket bucket = new Bucket();
-		bucket.userId = userId;
-		bucket.seller = seller;
-		bucket.productId = productId;
-		bucket.quantity = quantity;
-		return bucket;
+		this.id = id;
+		this.userId = userId;
+		this.seller = seller;
+		this.productId = productId;
+		this.quantity = quantity;
+		this.createDate = createDate;
+	}
+
+	public static Bucket ofAdd(
+			final Integer userId,
+			final String seller,
+			final Integer productId,
+			final Integer quantity
+	) {
+		return new Bucket(
+				null,
+				userId,
+				seller,
+				productId,
+				quantity,
+				null
+		);
 	}
 
 	// TODO : 상품 상세옵션 변경 로직 추가
