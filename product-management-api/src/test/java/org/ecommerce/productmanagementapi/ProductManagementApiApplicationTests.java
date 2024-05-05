a60363c4b9694cc75f2cb81e6421022138be6918
HongJongMin <gwa7417@gmail.com>
2024-05-05 20:01:10 +0900

    refactor: Import 추가

--- product-management-api/src/test/java/org/ecommerce/productmanagementapi/ProductManagementApiApplicationTests.java
+++ product-management-api/src/test/java/org/ecommerce/productmanagementapi/ProductManagementApiApplicationTests.java
@@ -1,15 +1,18 @@
 package org.ecommerce.productmanagementapi;
 
+import org.ecommerce.productmanagementapi.config.MockS3Config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = ProductManagementApiApplication.class)
+@Import(MockS3Config.class)
 @ActiveProfiles("test")
 class ProductManagementApiApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+	@Test
+	void contextLoads() {
+	}
 
 }
