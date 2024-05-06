d5cf7f2e9d94b2deda766dc79164db159d87faa0
HongJongMin <gwa7417@gmail.com>
2024-05-06 19:45:26 +0900

    refactor: 메서드 삭제

--- user-api/src/main/java/org/ecommerce/userapi/repository/UserCustomRepository.java
+++ user-api/src/main/java/org/ecommerce/userapi/repository/UserCustomRepository.java
@@ -1,9 +1,4 @@
 package org.ecommerce.userapi.repository;
 
-import java.util.Optional;
-
-import org.ecommerce.userapi.entity.Users;
-
 public interface UserCustomRepository {
-	Optional<Users> findUsersById(Integer id);
 }
--- user-api/src/main/java/org/ecommerce/userapi/repository/impl/UserRepositoryImpl.java
+++ user-api/src/main/java/org/ecommerce/userapi/repository/impl/UserRepositoryImpl.java
@@ -1,10 +1,5 @@
 package org.ecommerce.userapi.repository.impl;
 
-import static org.ecommerce.userapi.entity.QUsers.*;
-
-import java.util.Optional;
-
-import org.ecommerce.userapi.entity.Users;
 import org.ecommerce.userapi.repository.UserCustomRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,10 +13,4 @@ public class UserRepositoryImpl implements UserCustomRepository {
 
 	private final JPAQueryFactory jpaQueryFactory;
 
-	@Override
-	public Optional<Users> findUsersById(Integer id) {
-		return Optional.ofNullable(jpaQueryFactory.selectFrom(users)
-			.where(users.id.eq(id))
-			.fetchFirst());
-	}
 }
