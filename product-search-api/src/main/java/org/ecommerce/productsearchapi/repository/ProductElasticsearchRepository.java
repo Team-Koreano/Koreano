8d428bdf13362257b21435753ca112991786f8ff
no.oneho <oneho.dev@gmail.com>
2024-05-06 09:39:17 +0900

    Feat: es 와 jpa repo를 불러올 service 파일 분리 및 검색어 제안 API 개발

--- product-search-api/src/main/java/org/ecommerce/productsearchapi/repository/ProductElasticsearchRepository.java
+++ product-search-api/src/main/java/org/ecommerce/productsearchapi/repository/ProductElasticsearchRepository.java
@@ -1,7 +1,10 @@
 package org.ecommerce.productsearchapi.repository;
 
+import java.util.List;
+
 import org.ecommerce.productsearchapi.document.ProductDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ProductElasticsearchRepository extends ElasticsearchRepository<ProductDocument, Integer> {
+	List<ProductDocument> findByNameContaining(String keyword);
 }
