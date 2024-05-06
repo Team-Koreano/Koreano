a1532f7fbaea8e0e63fbcc4359dc6f065aefbac9
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-05-06 19:08:06 +0900

    Test : 주문 조회 Controller Test

--- order-api/src/test/java/org/ecommerce/orderapi/external/controller/OrderControllerTest.java
+++ order-api/src/test/java/org/ecommerce/orderapi/external/controller/OrderControllerTest.java
@@ -1,16 +1,20 @@
 package org.ecommerce.orderapi.external.controller;
 
+import static org.ecommerce.orderapi.entity.enumerated.OrderStatus.*;
 import static org.ecommerce.orderapi.exception.ErrorMessage.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.ecommerce.orderapi.dto.OrderDto;
+import org.ecommerce.orderapi.entity.OrderDetail;
+import org.ecommerce.orderapi.entity.OrderStatusHistory;
 import org.ecommerce.orderapi.service.OrderService;
 import org.ecommerce.orderapi.service.StockService;
 import org.junit.jupiter.api.Test;
@@ -53,7 +57,8 @@ public class OrderControllerTest {
 				"빠른 배송 부탁드려요.",
 				10000,
 				LocalDateTime.of(2024, 4, 22, 0, 1, 0, 1),
-				LocalDateTime.of(2024, 4, 22, 0, 2, 0, 1)
+				LocalDateTime.of(2024, 4, 22, 0, 2, 0, 1),
+				new ArrayList<>()
 		);
 		when(orderService.placeOrder(anyInt(), any(OrderDto.Request.Place.class)))
 				.thenReturn(orderDto);
@@ -75,12 +80,16 @@ public class OrderControllerTest {
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.result.id").value(orderDto.getId()))
 				.andExpect(jsonPath("$.result.userId").value(orderDto.getUserId()))
-				.andExpect(jsonPath("$.result.receiveName").value(orderDto.getReceiveName()))
-				.andExpect(jsonPath("$.result.phoneNumber").value(orderDto.getPhoneNumber()))
+				.andExpect(
+						jsonPath("$.result.receiveName").value(orderDto.getReceiveName()))
+				.andExpect(
+						jsonPath("$.result.phoneNumber").value(orderDto.getPhoneNumber()))
 				.andExpect(jsonPath("$.result.address1").value(orderDto.getAddress1()))
 				.andExpect(jsonPath("$.result.address2").value(orderDto.getAddress2()))
-				.andExpect(jsonPath("$.result.deliveryComment").value(orderDto.getDeliveryComment()))
-				.andExpect(jsonPath("$.result.totalPaymentAmount").value(orderDto.getTotalPaymentAmount()))
+				.andExpect(jsonPath("$.result.deliveryComment").value(
+						orderDto.getDeliveryComment()))
+				.andExpect(jsonPath("$.result.totalPaymentAmount").value(
+						orderDto.getTotalPaymentAmount()))
 				.andDo(print());
 	}
 
@@ -104,6 +113,80 @@ public class OrderControllerTest {
 				.andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.status").value(HttpStatus.BAD_REQUEST.value()))
 				.andExpect(jsonPath("$.result").value(ADDRESS1_NOT_BLANK));
+	}
+
+	@Test
+	void 주문내역_조회() throws Exception {
+		// given
+		final Integer userId = 1;
+		final Integer year = null;
+		final Integer pageNumber = 1;
+		List<OrderDto> orderDtos = List.of(
+				new OrderDto(
+						1L,
+						1,
+						"receiveName",
+						"010-777-7777",
+						"동백",
+						"백동",
+						"빠른 배송 부탁드려요.",
+						10000,
+						LocalDateTime.of(2024, 4, 22, 0, 1, 0, 1),
+						LocalDateTime.of(2024, 4, 22, 0, 2, 0, 1),
+						List.of(
+								new OrderDetail(
+										1L,
+										null,
+										101,
+										10000,
+										1,
+										10000,
+										0,
+										10000,
+										"seller1",
+										OPEN,
+										null,
+										List.of(
+												new OrderStatusHistory(
+														1L,
+														null,
+														OPEN,
+														LocalDateTime.of(2024, 5, 6, 0, 0)
+												)
+										)
+
+								)
+						)
+				)
+		);
+		given(orderService.getOrders(userId, year, pageNumber))
+				.willReturn(orderDtos);
+
+		// when
+		// then
+		OrderDto orderDto = orderDtos.get(0);
+		OrderDetail orderDetail = orderDto.getOrderDetails().get(0);
+		OrderStatusHistory orderStatusHistory = orderDetail.getOrderStatusHistories()
+				.get(0);
+		mockMvc.perform(get("/api/external/orders/v1?year=&pageNumber=1"))
+				.andDo(print())
+				.andExpect(jsonPath("$.result.[0].id").value(orderDto.getId()))
+				.andExpect(jsonPath("$.result.[0].userId").value(orderDto.getUserId()))
+				.andExpect(jsonPath("$.result.[0].receiveName")
+						.value(orderDto.getReceiveName()))
+				.andExpect(jsonPath("$.result.[0].address1")
+						.value(orderDto.getAddress1()))
+				.andExpect(jsonPath("$.result.[0].address2")
+						.value(orderDto.getAddress2()))
+				.andExpect(jsonPath("$.result.[0].deliveryComment")
+						.value(orderDto.getDeliveryComment()))
+				.andExpect(jsonPath("$.result.[0].orderDatetime")
+						.value(orderDto.getOrderDatetime().toString()))
+				.andExpect(jsonPath("$.result.[0].orderDetails[0].status")
+						.value(orderDetail.getStatus().toString()))
+				.andExpect(jsonPath(
+						"$.result.[0].orderDetails.[0].orderStatusHistories.[0].changeStatus")
+						.value(orderStatusHistory.getChangeStatus().toString()));
 	}
 
 }
