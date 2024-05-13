2c3acc70d211a5794f181ef72a6fbd940c50e2a2
HongJongMin <gwa7417@gmail.com>
2024-05-13 21:47:22 +0900

    refactor: 에러코드추가

--- product-management-api/src/main/java/org/ecommerce/productmanagementapi/exception/ProductManagementErrorCode.java
+++ product-management-api/src/main/java/org/ecommerce/productmanagementapi/exception/ProductManagementErrorCode.java
@@ -13,7 +13,8 @@ public enum ProductManagementErrorCode implements ErrorCode {
 	CAN_NOT_BE_SET_TO_BELOW_ZERO("0 보다 적게 설정 할 수 없습니다.", HttpStatus.BAD_REQUEST.value()),
 	NOT_FOUND_SELLER("판매자를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()),
 	IS_INVALID_FILE_OPTION("유효하지않은 확장자 입니다.", HttpStatus.BAD_REQUEST.value()),
-	FAILED_FILE_UPLOAD("파일 업로드에 실패하였습니다.", HttpStatus.BAD_REQUEST.value());
+	FAILED_FILE_UPLOAD("파일 업로드에 실패하였습니다.", HttpStatus.BAD_REQUEST.value()),
+	NOT_FOUND_CATEGORY("해당 카테고리를 찾을 수 없습니다", HttpStatus.BAD_REQUEST.value());
 
 	ProductManagementErrorCode(String message, int code) {
 		this.message = message;
