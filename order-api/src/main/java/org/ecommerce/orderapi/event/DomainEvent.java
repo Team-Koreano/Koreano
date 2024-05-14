6425ea01e22b790f52fbea00f670a14b18d39dca
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-05-14 21:38:53 +0900

    Refactor : Event 수정

--- order-api/src/main/java/org/ecommerce/orderapi/event/DomainEvent.java
+++ order-api/src/main/java/org/ecommerce/orderapi/event/DomainEvent.java
@@ -2,4 +2,5 @@ package org.ecommerce.orderapi.event;
 
 public interface DomainEvent<T> {
 	// TODO : COMMON 이전
+	T getDomainId();
 }
--- order-api/src/main/java/org/ecommerce/orderapi/event/OrderCreatedEvent.java
+++ order-api/src/main/java/org/ecommerce/orderapi/event/OrderCreatedEvent.java
@@ -2,10 +2,8 @@ package org.ecommerce.orderapi.event;
 
 import java.time.LocalDateTime;
 
-import org.ecommerce.orderapi.dto.OrderDto;
-
 public class OrderCreatedEvent extends OrderEvent {
-	public OrderCreatedEvent(OrderDto orderDto, LocalDateTime createdAt) {
-		super(orderDto, createdAt);
+	public OrderCreatedEvent(final Long orderId, final LocalDateTime createdAt) {
+		super(orderId, createdAt);
 	}
 }
