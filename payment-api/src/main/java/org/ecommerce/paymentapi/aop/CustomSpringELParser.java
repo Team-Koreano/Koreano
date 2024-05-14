f6d242fcde8b1621cc3405af585e5e360bd2af6e
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-05-14 23:17:25 +0900

    Feat: 멀티 가변락 작성

--- payment-api/src/main/java/org/ecommerce/paymentapi/aop/CustomSpringELParser.java
+++ payment-api/src/main/java/org/ecommerce/paymentapi/aop/CustomSpringELParser.java
@@ -1,23 +1,42 @@
 package org.ecommerce.paymentapi.aop;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-
 public class CustomSpringELParser {
+	private final static String getExpression = ".get()";
 	private CustomSpringELParser() {
 	}
 
-	public static Object getDynamicValue(String[] parameterNames, Object[] args,
+	public static Object[] getDynamicValue(String[] parameterNames, Object[] args,
 		String key) {
 		ExpressionParser parser = new SpelExpressionParser();
 		StandardEvaluationContext context = new StandardEvaluationContext();
+		List<Object> returnKeys = new LinkedList<>();
 
 		for (int i = 0; i < parameterNames.length; i++) {
 			context.setVariable(parameterNames[i], args[i]);
 		}
 
-		return parser.parseExpression(key).getValue(context, Object.class);
+		try{
+			int listSize = parser.parseExpression(
+					key.substring(0, key.indexOf(getExpression)))
+				.getValue(context, List.class)
+				.size();
+
+			for(int i = 0; i < listSize; i++){
+				String newKey = key.replace(getExpression, ".get("+ i +")");
+				returnKeys.add(parser.parseExpression(newKey).getValue(context, Object.class));
+			}
+
+		}catch(Exception e){
+			returnKeys.add(parser.parseExpression(key).getValue(context, Object.class));
+		}finally{
+			return returnKeys.toArray();
+		}
 	}
 }
--- payment-api/src/main/java/org/ecommerce/paymentapi/aop/DistributedLock.java
+++ payment-api/src/main/java/org/ecommerce/paymentapi/aop/DistributedLock.java
@@ -1,16 +1,21 @@
 package org.ecommerce.paymentapi.aop;
 
+import static org.ecommerce.paymentapi.entity.enumerate.LockName.*;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
+import org.ecommerce.paymentapi.entity.enumerate.LockName;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DistributedLock {
 
+
+	LockName lockName() default BEANPAY;
 
 	String[] key();
 
