77a06b07790400814ca49ba201809bc453453d4a
HongJongMin <gwa7417@gmail.com>
2024-05-15 17:16:01 +0900

    refactor: management queryDsl 추가

--- product-management-api/src/main/java/org/ecommerce/productmanagementapi/repository/ProductCustomRepository.java
+++ product-management-api/src/main/java/org/ecommerce/productmanagementapi/repository/ProductCustomRepository.java
@@ -1,4 +1,9 @@
 package org.ecommerce.productmanagementapi.repository;
 
+import java.util.List;
+
+import org.ecommerce.product.entity.Product;
+
 public interface ProductCustomRepository {
+	List<Product> findProductById(List<Integer> productIds);
 }
