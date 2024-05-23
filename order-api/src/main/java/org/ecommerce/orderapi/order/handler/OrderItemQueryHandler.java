fae0dbc99b9759509b9121bb7189c91e00af91a5
Joowon-Seo <tjwndnjs1998@gmail.com>
2024-05-23 20:26:20 +0900

    Docs : javaDoc 추가

--- order-api/src/main/java/org/ecommerce/orderapi/order/handler/OrderItemQueryHandler.java
+++ order-api/src/main/java/org/ecommerce/orderapi/order/handler/OrderItemQueryHandler.java
@@ -37,6 +37,20 @@ public class OrderItemQueryHandler {
 		);
 	}
 
+	/**
+	 * 주문 항목 리스트를 조회하는 메소드입니다.
+	 * <p>
+	 * seller가 주문 항목 리스트를 조회할 때 사용합니다.
+	 * month는 조회할 기간입니다. default 이번 달
+	 * pageSize 10으로 고정인 상태이고 추후 동적으로 변경할 예정입니다.
+	 * <p>
+	 * @author ${Juwon}
+	 *
+	 * @param sellerId- 판매자 번호
+	 * @param month- 조회 기간
+	 * @param pageNumber- 페이지 번호
+	 * @return - 주문 항목 리스트
+	 */
 	public List<OrderItemDto> getOrderItems(
 			final Integer sellerId,
 			final Integer month,
