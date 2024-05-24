7d6769d49b707d96c824cadf7d6f78dcb2bdad94
HongJongMin <gwa7417@gmail.com>
2024-05-24 17:54:25 +0900

    refactor: Optional 제거 메서드명 수정

--- product-management-api/src/main/java/org/ecommerce/productmanagementapi/repository/impl/ProductRepositoryImpl.java
+++ product-management-api/src/main/java/org/ecommerce/productmanagementapi/repository/impl/ProductRepositoryImpl.java
@@ -3,7 +3,6 @@ package org.ecommerce.productmanagementapi.repository.impl;
 import static org.ecommerce.product.entity.QProduct.*;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.ecommerce.product.entity.Product;
 import org.ecommerce.productmanagementapi.repository.ProductCustomRepository;
@@ -18,24 +17,20 @@ public class ProductRepositoryImpl implements ProductCustomRepository {
 	private final JPAQueryFactory jpaQueryFactory;
 
 	@Override
-	public List<Product> findProductsById(List<Integer> productIds) {
+	public List<Product> findProductsByIds(List<Integer> productIds) {
 		return jpaQueryFactory.selectFrom(product)
-			.leftJoin(product.images)
-			.fetchJoin()
-			.leftJoin(product.sellerRep)
-			.fetchJoin()
+			.leftJoin(product.images).fetchJoin()
+			.leftJoin(product.sellerRep).fetchJoin()
 			.where(product.id.in(productIds))
 			.fetch();
 	}
 
 	@Override
-	public Optional<Product> findProductById(Integer productId) {
-		return Optional.ofNullable(jpaQueryFactory.selectFrom(product)
-			.leftJoin(product.images)
-			.fetchJoin()
-			.leftJoin(product.sellerRep)
-			.fetchJoin()
+	public Product findProductById(Integer productId) {
+		return jpaQueryFactory.selectFrom(product)
+			.leftJoin(product.images).fetchJoin()
+			.leftJoin(product.sellerRep).fetchJoin()
 			.where(product.id.eq(productId))
-			.fetchFirst());
+			.fetchFirst();
 	}
 }
