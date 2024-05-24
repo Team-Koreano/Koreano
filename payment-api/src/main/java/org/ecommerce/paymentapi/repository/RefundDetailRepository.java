ab82068dbab159f70d09fbc09628f01691a4b55b
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-05-24 15:55:41 +0900

    Refactor: 사용하지 않는 Entity 제거

--- payment-api/src/main/java/org/ecommerce/paymentapi/repository/RefundDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ecommerce.paymentapi.repository;
-
-import org.ecommerce.paymentapi.entity.RefundDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RefundDetailRepository extends JpaRepository<RefundDetail, Long> {
-}
