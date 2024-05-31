6cb55875311da9b481804397475274b3e826bae2
HongJongMin <gwa7417@gmail.com>
2024-05-31 15:11:41 +0900

    refactor: Validation 추가

--- user-api/src/main/java/org/ecommerce/userapi/dto/request/CreateBeanPayRequest.java
+++ user-api/src/main/java/org/ecommerce/userapi/dto/request/CreateBeanPayRequest.java
@@ -1,8 +1,14 @@
 package org.ecommerce.userapi.dto.request;
 
 import org.ecommerce.userapi.entity.enumerated.Role;
+import org.ecommerce.userapi.exception.UserErrorMessages;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateBeanPayRequest(
+	@NotNull(message = UserErrorMessages.IdIsNotNull)
+	@Min(value = 1, message = UserErrorMessages.IsCanNotBeBelowZero)
 	Integer id,
 	Role role
 ) {
--- user-api/src/main/java/org/ecommerce/userapi/dto/request/CreateUserRequest.java
+++ user-api/src/main/java/org/ecommerce/userapi/dto/request/CreateUserRequest.java
@@ -4,6 +4,7 @@ import org.ecommerce.userapi.entity.enumerated.Gender;
 import org.ecommerce.userapi.exception.UserErrorMessages;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -17,6 +18,7 @@ public record CreateUserRequest(
 	String password,
 	Gender gender,
 	@NotNull(message = UserErrorMessages.ageNotNull)
+	@Min(value = 1, message = UserErrorMessages.IsCanNotBeBelowZero)
 	Short age,
 	@NotBlank(message = UserErrorMessages.phoneNumberNotBlank)
 	String phoneNumber
