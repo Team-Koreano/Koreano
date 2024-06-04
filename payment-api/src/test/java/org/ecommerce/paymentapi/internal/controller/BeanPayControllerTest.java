2efadc4bd65942f262f614207272a402edda7e46
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-06-04 17:01:42 +0900

    Test: 회원, 판매자 삭제 코드 작성 및 수정

--- payment-api/src/test/java/org/ecommerce/paymentapi/internal/controller/BeanPayControllerTest.java
+++ payment-api/src/test/java/org/ecommerce/paymentapi/internal/controller/BeanPayControllerTest.java
@@ -7,9 +7,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.ecommerce.paymentapi.dto.SellerBeanPayDto;
+import org.ecommerce.paymentapi.dto.SellerBeanPayMapper;
 import org.ecommerce.paymentapi.dto.UserBeanPayDto;
 import org.ecommerce.paymentapi.dto.UserBeanPayMapper;
 import org.ecommerce.paymentapi.dto.request.CreateUserBeanPayRequest;
+import org.ecommerce.paymentapi.dto.request.DeleteSellerBeanPayRequest;
+import org.ecommerce.paymentapi.dto.request.DeleteUserBeanPayRequest;
+import org.ecommerce.paymentapi.entity.SellerBeanPay;
 import org.ecommerce.paymentapi.entity.UserBeanPay;
 import org.ecommerce.paymentapi.internal.service.BeanPayService;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,7 +33,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(BeanPayController.class)
 @MockBean(JpaMetamodelMappingContext.class)
-class UserBeanPayControllerTest {
+class BeanPayControllerTest {
 
 	@MockBean
 	private BeanPayService beanPayService;
@@ -50,7 +55,7 @@ class UserBeanPayControllerTest {
 	}
 
 	@Nested
-	class 빈페이_생성 {
+	class 유저_빈페이_생성 {
 		@Test
 		void 성공() throws Exception {
 			//given
@@ -70,5 +75,66 @@ class UserBeanPayControllerTest {
 				.andExpect(status().isOk());
 		}
 	}
+	@Nested
+	class 유저_빈페이_삭제 {
+		@Test
+		void 성공() throws Exception {
+			//given
+			final Integer userId = 1;
+			final DeleteUserBeanPayRequest request = new DeleteUserBeanPayRequest(userId);
+
+			//when
+			doNothing().when(beanPayService).deleteUserBeanPay(request);
+
+			//then
+			mvc.perform(delete("/api/internal/beanpay/v1/user")
+					.contentType(APPLICATION_JSON)
+					.content(mapper.writeValueAsString(request)))
+				.andDo(print())
+				.andExpect(status().isOk());
+		}
+	}
+	@Nested
+	class 판매자_빈페이_생성 {
+		@Test
+		void 성공() throws Exception {
+			//given
+			final Integer sellerId = 1;
+			final CreateUserBeanPayRequest request = new CreateUserBeanPayRequest(sellerId);
+			final SellerBeanPay sellerBeanPay = SellerBeanPay.ofCreate(sellerId);
+			final SellerBeanPayDto sellerBeanPayDto =
+				SellerBeanPayMapper.INSTANCE.toDto(sellerBeanPay);
+
+			//when
+			doNothing().when(beanPayService).createUserBeanPay(request);
+
+			//then
+			mvc.perform(post("/api/internal/beanpay/v1/seller")
+					.contentType(APPLICATION_JSON)
+					.content(mapper.writeValueAsString(request)))
+				.andDo(print())
+				.andExpect(status().isOk());
+		}
+	}
+	@Nested
+	class 판매자_빈페이_삭제 {
+		@Test
+		void 성공() throws Exception {
+			//given
+			final Integer userId = 1;
+			final DeleteSellerBeanPayRequest request = new DeleteSellerBeanPayRequest(userId);
+
+			//when
+			doNothing().when(beanPayService).deleteSellerBeanPay(request);
+
+			//then
+			mvc.perform(delete("/api/internal/beanpay/v1/seller")
+					.contentType(APPLICATION_JSON)
+					.content(mapper.writeValueAsString(request)))
+				.andDo(print())
+				.andExpect(status().isOk());
+		}
+	}
+
 
 }
