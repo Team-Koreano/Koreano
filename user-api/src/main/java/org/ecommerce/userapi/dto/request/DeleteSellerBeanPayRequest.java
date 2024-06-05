c37abd51f4ab246aa1310d599ab60ff77c747b67
HongJongMin <gwa7417@gmail.com>
2024-06-05 18:21:00 +0900

    refactor: 셀러와 유저 각각 Request 생성

--- user-api/src/main/java/org/ecommerce/userapi/dto/request/DeleteSellerBeanPayRequest.java
+++ user-api/src/main/java/org/ecommerce/userapi/dto/request/DeleteSellerBeanPayRequest.java
@@ -1,15 +1,13 @@
 package org.ecommerce.userapi.dto.request;
 
-import org.ecommerce.userapi.entity.enumerated.Role;
 import org.ecommerce.userapi.exception.UserErrorMessages;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-public record DeleteBeanPayRequest(
+public record DeleteSellerBeanPayRequest(
 	@NotNull(message = UserErrorMessages.IdIsNotNull)
 	@Min(value = 1, message = UserErrorMessages.IsCanNotBeBelowZero)
-	Integer id,
-	Role role
+	Integer sellerId
 ) {
 }
