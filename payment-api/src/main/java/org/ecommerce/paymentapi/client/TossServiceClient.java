f8e095ef3e668780abc25ff4e19776718a36eaad
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-06-06 16:26:44 +0900

    Chore: 운영 Port번호 설정 및 설정 파일

--- payment-api/src/main/java/org/ecommerce/paymentapi/client/TossServiceClient.java
+++ payment-api/src/main/java/org/ecommerce/paymentapi/client/TossServiceClient.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "toss", url = "https://api.tosspayments.com/v1/payments")
+
+@FeignClient(name = "toss", url = "${toss.server.url}")
 public interface TossServiceClient {
 
 	@PostMapping("/confirm")
