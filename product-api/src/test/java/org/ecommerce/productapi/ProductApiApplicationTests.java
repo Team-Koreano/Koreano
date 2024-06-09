4c2635986252e84972b415ff5012a65594b9f515
HongJongMin <gwa7417@gmail.com>
2024-06-09 18:43:29 +0900

    feat: 테스트코드 수정

--- product-api/src/test/java/org/ecommerce/productapi/ProductApiApplicationTests.java
+++ product-api/src/test/java/org/ecommerce/productapi/ProductApiApplicationTests.java
@@ -1,8 +1,10 @@
 package org.ecommerce.productapi;
 
 import org.ecommerce.productapi.config.MockS3Config;
+import org.ecommerce.productapi.repository.ProductElasticsearchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -10,6 +12,9 @@ import org.springframework.test.context.ActiveProfiles;
 @Import(MockS3Config.class)
 @ActiveProfiles("test")
 class ProductApiApplicationTests {
+	
+	@MockBean
+	private ProductElasticsearchRepository productElasticsearchRepository;
 
 	@Test
 	void contextLoads() {
