76d376dd5f2c9a6f2c871e57a38050a6b97a3676
HongJongMin <gwa7417@gmail.com>
2024-06-09 18:29:17 +0900

    feat: 에러코드 추가

--- product-api/src/main/java/org/ecommerce/productapi/exception/ProductErrorCode.java
+++ product-api/src/main/java/org/ecommerce/productapi/exception/ProductErrorCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 @Getter
 public enum ProductErrorCode implements ErrorCode {
 	NOT_FOUND_PRODUCT("상품을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()),
+	NOT_FOUND_PRODUCT_DETAIL("상세 상품을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()),
 	IS_EQUAL_TO_NOW_STATUS("현재 상품의 상태값과 똑같습니다.", HttpStatus.BAD_REQUEST.value()),
 	NOT_FOUND_STATUS("상태값을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()),
 	CAN_NOT_BE_SET_TO_BELOW_ZERO("0 보다 적게 설정 할 수 없습니다.", HttpStatus.BAD_REQUEST.value()),
@@ -16,8 +17,8 @@ public enum ProductErrorCode implements ErrorCode {
 	FAILED_FILE_UPLOAD("파일 업로드에 실패하였습니다.", HttpStatus.BAD_REQUEST.value()),
 	NOT_FOUND_CATEGORY("해당 카테고리를 찾을 수 없습니다", HttpStatus.BAD_REQUEST.value()),
 	FAILED_PRODUCT_UPLOAD("상품 등록에 실패하였습니다.", HttpStatus.BAD_REQUEST.value()),
-	NOT_FOUND_PRODUCT_ID( "존재하지 않는 상품 ID 입니다.", HttpStatus.BAD_REQUEST.value());
-
+	NOT_FOUND_PRODUCT_ID("존재하지 않는 상품 ID 입니다.", HttpStatus.BAD_REQUEST.value()),
+	IS_NOT_ENOUGH_PRODUCT_DETAIL("최소 한개 이상의 상품 상세가 존재해야 합니다.", HttpStatus.BAD_REQUEST.value());
 
 	ProductErrorCode(String message, int code) {
 		this.message = message;
