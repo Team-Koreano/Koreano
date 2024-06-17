2bb17b41da731573fce1ea366ca894f42c424a04
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2024-06-18 00:53:12 +0900

    Refactor: 레디스 임베디드 클러스터로 변경

--- payment-api/src/test/java/org/ecommerce/paymentapi/config/LocalRedisConfig.java
+++ payment-api/src/test/java/org/ecommerce/paymentapi/config/LocalRedisConfig.java
@@ -1,24 +1,28 @@
 package org.ecommerce.paymentapi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.context.annotation.Configuration;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import redis.embedded.RedisServer;
+import redis.embedded.Redis;
+import redis.embedded.RedisCluster;
 
 @Configuration
 public class LocalRedisConfig{
+	private final int START_REDIS_PORT = 1000;
+	private final int CLUSTER_AMOUNT = 3;
+	private final Redis redisServer;
 
-	private final RedisProperties redisProperties;
-
-	private final RedisServer redisServer;
-
-
-	@Autowired
-	public LocalRedisConfig(RedisProperties properties) {
-		this.redisProperties = properties;
-		this.redisServer = new RedisServer(redisProperties.getPort());
+	public LocalRedisConfig() {
+		this.redisServer = RedisCluster.builder()
+			.serverPorts(
+				IntStream.range(START_REDIS_PORT, START_REDIS_PORT + CLUSTER_AMOUNT)
+				.boxed()
+				.collect(Collectors.toList()))
+			.build();
 	}
 
 	@PostConstruct
