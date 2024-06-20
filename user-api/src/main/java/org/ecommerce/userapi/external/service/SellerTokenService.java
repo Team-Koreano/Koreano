d34952fdeca727fffea32c8a60d5cdd56d232e8f
HongJongMin <gwa7417@gmail.com>
2024-06-20 17:09:20 +0900

    feat: redisProvider 와 jwtProvider 결합 분리
    [해당 분리로 인한 UserTokenService 와 SellerTokenService 를 추가하여 Common 영역에서의 Redis를 제외]

---
+++ user-api/src/main/java/org/ecommerce/userapi/external/service/SellerTokenService.java
@@ -0,0 +1,50 @@
+package org.ecommerce.userapi.external.service;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import javax.crypto.SecretKey;
+
+import org.ecommerce.common.provider.JwtProvider;
+import org.ecommerce.userapi.provider.RedisProvider;
+import org.springframework.stereotype.Service;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SellerTokenService {
+	private static final long ONE_HOUR = 3_600;
+	private static final long TWO_WEEKS = ONE_HOUR * 24 * 14;
+
+	private final SecretKey secretKey;
+
+	private final RedisProvider redisProvider;
+
+	private final JwtProvider jwtProvider;
+
+	public String createSellerTokens(Integer sellerId, Set<String> authorization,
+		HttpServletResponse response) {
+
+		final String accessToken = jwtProvider.createToken(sellerId,
+			ONE_HOUR, secretKey, authorization);
+		final String refreshToken = jwtProvider.createToken(sellerId,
+			TWO_WEEKS, secretKey, authorization);
+
+		String accessTokenKey = jwtProvider.getAccessTokenKey(sellerId, jwtProvider.getRoll(accessToken));
+		redisProvider.setData(accessTokenKey, accessToken, ONE_HOUR, TimeUnit.SECONDS);
+
+		String refreshTokenKey = jwtProvider.getRefreshTokenKey(sellerId, jwtProvider.getRoll(refreshToken));
+		redisProvider.setData(refreshTokenKey, refreshToken, TWO_WEEKS, TimeUnit.SECONDS);
+
+		response.addCookie(jwtProvider.createCookie(refreshToken));
+
+		return accessToken;
+	}
+
+	public void removeTokens(String accessTokenKey, String refreshTokenKey) {
+		redisProvider.deleteData(accessTokenKey);
+		redisProvider.deleteData(refreshTokenKey);
+	}
+}
