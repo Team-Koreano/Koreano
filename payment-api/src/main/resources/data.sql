c89c4eef7798bbdde069336ba0f060552a14ea67
HongJongMin <gwa7417@gmail.com>
2024-06-20 19:11:16 +0900

    feat: dataSQl 수정

--- payment-api/src/main/resources/data.sql
+++ payment-api/src/main/resources/data.sql
@@ -1,3 +1,2 @@
-INSERT INTO beanpay (id, user_id, role, amount, create_date_time, delete_date_time)
-VALUES (999, 999, 'USER', 100000, now(), null),
-       (1000, 1000, 'SELLER', 0, now(), null);
+INSERT INTO seller_beanpay (id, role, amount, create_date_time, delete_date_time)
+VALUES (1000, 1000, 0, now(), null);
